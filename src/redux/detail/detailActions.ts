fa1e0f45791db64997bf123ab91db58b38eacfb1
Bert Verhelst <verhelstbert@gmail.com>
2019-06-25 17:37:18 +0200

    fix: use queryString package to build queryparams for detail api request

--- src/redux/detail/detailActions.ts
+++ src/redux/detail/detailActions.ts
@@ -1,8 +1,9 @@
+import queryString from 'query-string';
 import { DetailResponse } from '../../types/detailTypes';
 
 export async function getDetail(id: string): Promise<DetailResponse> {
 	const url = `${process.env.REACT_APP_PROXY_URL}/detail`;
-	return fetch(`${url}?id=${id}`, {
+	return fetch(`${url}?${queryString.stringify({ id })}`, {
 		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json',
