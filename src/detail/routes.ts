8f41141592e3ceae0f7c568be9cbc0afa8d6aa73
Enzo Eghermanne <enzo.eghermanne@gmail.com>
2019-06-27 15:08:12 +0200

    feat: modulized routes

---
+++ src/detail/routes.ts
@@ -0,0 +1,9 @@
+import { Detail } from './views/Detail';
+
+export const DETAIL_ROUTES = [
+	{
+		path: '/detail/:id',
+		exact: true,
+		component: Detail,
+	},
+];
---
+++ src/routes.ts
@@ -0,0 +1,4 @@
+import { DETAIL_ROUTES } from './detail/routes';
+import { SEARCH_ROUTES } from './search/routes';
+
+export const ROUTES = [...SEARCH_ROUTES, ...DETAIL_ROUTES];
