b0ca6088cde2710976e487b37df8abb09797c0e7
Enzo Eghermanne <enzo.eghermanne@gmail.com>
2019-07-02 15:13:43 +0200

    test: added reducer tests

---
+++ src/search/store/reducer.test.ts
@@ -0,0 +1,31 @@
+import configureStore from 'redux-mock-store';
+
+import { SetSearchResultsError, SetSearchResultsLoading, SetSearchResultsSuccess } from './actions';
+import initialState from './initial-state';
+import { SearchActionTypes } from './types';
+
+import searchReducer from './reducer';
+
+describe('search > store > reducer', () => {
+	const mockStore = configureStore();
+
+	it(`Correctly handle ${SearchActionTypes.SET_RESULTS_ERROR}`, () => {
+		const state = searchReducer(initialState, SetSearchResultsError());
+
+		expect(state.error).toEqual(true);
+	});
+
+	it(`Correctly handle ${SearchActionTypes.SET_RESULTS_LOADING}`, () => {
+		const state = searchReducer(initialState, SetSearchResultsLoading());
+
+		expect(state.loading).toEqual(true);
+	});
+
+	it(`Correctly handle ${SearchActionTypes.SET_RESULTS_SUCCESS}`, () => {
+		const payload = { results: [], count: 0, aggregations: {} };
+
+		const state = searchReducer(initialState, SetSearchResultsSuccess(payload));
+
+		expect(state.data).toEqual(payload);
+	});
+});
