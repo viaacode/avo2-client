b2c6f63fc965da2df885eecdcaee23c25fbed0e4
Bert Verhelst <verhelstbert@gmail.com>
2019-07-19 09:05:39 +0200

    fix: add loginState reducer to src/store.ts

--- src/store.ts
+++ src/store.ts
@@ -2,6 +2,7 @@ import { applyMiddleware, combineReducers, createStore, Reducer } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
+import checkLoginStateReducer from './authentication/store/reducer';
 import itemReducer from './item/store/reducer';
 import searchReducer from './search/store/reducer';
 
@@ -11,6 +12,7 @@ export default createStore(
 	combineReducers<Reducer>({
 		search: searchReducer,
 		item: itemReducer,
+		loginState: checkLoginStateReducer,
 	}),
 	composeWithDevTools(applyMiddleware(...middleware))
 );
