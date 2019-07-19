85878f13f57c09d215922289db9f7d35559c4502
Bert Verhelst <verhelstbert@gmail.com>
2019-07-19 10:43:38 +0200

    fix: use container with custom className in NotFound component

--- src/404/views/NotFound.tsx
+++ src/404/views/NotFound.tsx
@@ -3,14 +3,13 @@ import React, { FunctionComponent } from 'react';
 
 interface NotFoundProps {}
 
-export const NotFound: FunctionComponent<NotFoundProps> = ({}) => {
+export const NotFound: FunctionComponent<NotFoundProps> = () => {
 	return (
-		<div className="o-container-vertical o-container-vertical-title o-container-vertical--bg-alt">
-			{/* TODO use Container component with custom class param after merging: https://github.com/viaacode/avo2-components/pull/67 */}
+		<Container mode="vertical" background="alt" className="o-container-vertical-title">
 			<Container size="medium" mode="horizontal">
 				<Blankslate body="" icon="search" title="De pagina werd niet gevonden" />
 			</Container>
-		</div>
+		</Container>
 	);
 };
 
