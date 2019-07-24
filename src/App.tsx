0436876db6f0246b2773ee4bda7b88172d00511f
Enzo Eghermanne <enzo.eghermanne@gmail.com>
2019-07-24 12:49:41 +0200

    feat: close menu when changing route

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,62 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { Fragment, FunctionComponent, useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
 
 import { renderRoutes } from 'react-router-config';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, RouteComponentProps, withRouter } from 'react-router-dom';
 
 import { Navigation } from './shared/components/Navigation/Navigation';
 
 import { ROUTES } from './routes';
 import store from './store';
 
-const App: FunctionComponent = () => {
+const App: FunctionComponent<RouteComponentProps> = ({ history }) => {
 	const [menuOpen, setMenuOpen] = useState(false);
 
+	useEffect(() => {
+		return history.listen(closeMenu);
+	});
+
+	const toggleMenu = () => {
+		setMenuOpen(!menuOpen);
+	};
+
+	const closeMenu = () => {
+		setMenuOpen(false);
+	};
+
+	return (
+		<Fragment>
+			<Navigation
+				primaryItems={[
+					{ label: 'Home', location: '/' },
+					{ label: 'Zoeken', location: 'search' },
+					{ label: 'Ontdek', location: '/' },
+					{ label: 'Mijn Archief', location: '/mijn-werkruimte/collecties' },
+					{ label: 'Projecten', location: '/' },
+					{ label: 'Nieuws', location: '/' },
+				]}
+				secondaryItems={[
+					{ label: 'Registreren', location: '/' },
+					{ label: 'Aanmelden', location: '/' },
+				]}
+				isOpen={menuOpen}
+				handleMenuClick={toggleMenu}
+			/>
+			{renderRoutes(ROUTES)}
+		</Fragment>
+	);
+};
+
+const AppWithRouter = withRouter(App);
+
+const Root: FunctionComponent = () => {
 	return (
 		<Provider store={store}>
 			<Router>
-				<Navigation
-					primaryItems={[
-						{ label: 'Home', location: '/' },
-						{ label: 'Zoeken', location: 'search' },
-						{ label: 'Ontdek', location: '/' },
-						{ label: 'Mijn Archief', location: '/mijn-werkruimte/collecties' },
-						{ label: 'Projecten', location: '/' },
-						{ label: 'Nieuws', location: '/' },
-					]}
-					secondaryItems={[
-						{ label: 'Registreren', location: '/' },
-						{ label: 'Aanmelden', location: '/' },
-					]}
-					isOpen={menuOpen}
-					handleMenuClick={() => setMenuOpen(!menuOpen)}
-				/>
-				{renderRoutes(ROUTES)}
+				<AppWithRouter />
 			</Router>
 		</Provider>
 	);
 };
 
-export default App;
+export default Root;
