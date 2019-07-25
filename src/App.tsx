c29edd54024fc58c2fbe5dad990b8b17ce878fd4
Andry Charlier <andrycharlier@gmail.com>
2019-07-25 14:46:24 +0200

    feat: add react-toastify package and cleanup @types

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { FunctionComponent } from 'react';
 import { Provider } from 'react-redux';
 
-import { renderRoutes } from 'react-router-config';
+import { renderRoutes, RouteConfig } from 'react-router-config';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import { ROUTES } from './routes';
 import store from './store';
@@ -10,13 +11,14 @@ import store from './store';
 const App: FunctionComponent = () => {
 	return (
 		<Provider store={store}>
+			<ToastContainer />
 			<h1>Archief voor Onderwijs Homepage</h1>
 			<Router>
 				<Link to="/">Home</Link>&nbsp;
 				<Link to="/search">Search</Link>&nbsp;
 				<Link to="/collection/1725151">Collection</Link>&nbsp;
 				<Link to="/mijn-werkruimte/collecties">Mijn Werkruimte</Link>
-				{renderRoutes(ROUTES)}
+				{renderRoutes(ROUTES as RouteConfig[])}
 			</Router>
 		</Provider>
 	);
