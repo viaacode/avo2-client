9b9799311a25d95d4f796fdce99f72080fe537f5
Bert Verhelst <verhelstbert@gmail.com>
2019-07-29 10:45:38 +0200

    chore: remove ruminant of moved file

--- src/my-workspace/views/Collections.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Fragment, FunctionComponent } from 'react';
-
-import { RouteComponentProps } from 'react-router';
-
-interface CollectionsProps extends RouteComponentProps {}
-
-export const Collections: FunctionComponent<CollectionsProps> = () => {
-	return <Fragment />;
-};
