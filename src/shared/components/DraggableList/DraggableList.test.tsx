8ee91252b7c672574b8decd9dbad297b31c21614
Benjamin Naesen <benjamin.naesen@studiohyperdrive.be>
2019-08-01 10:44:29 +0200

    feat: added tests for draggable-list

--- src/shared/components/DraggableList/DraggableList.test.tsx
+++ src/shared/components/DraggableList/DraggableList.test.tsx
@@ -9,4 +9,22 @@ describe('<DraggableList />', () => {
 	it('Should be able to render', () => {
 		shallow(<DraggableList elements={mockElements} onListChange={mockElements => {}} />);
 	});
+
+	it('Should set the correct className', () => {
+		const draggableListComponent = shallow(
+			<DraggableList elements={mockElements} onListChange={mockElements => {}} />
+		);
+
+		expect(draggableListComponent.hasClass('c-table-view')).toEqual(true);
+	});
+
+	it('Should correctly pass children', () => {
+		const draggableListComponent = mount(
+			<DraggableList elements={mockElements} onListChange={mockElements => {}} />
+		);
+
+		const draggableListItems = draggableListComponent.find('.c-table-view__item');
+
+		expect(draggableListItems).toHaveLength(mockElements.length);
+	});
 });
