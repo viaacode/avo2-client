8e3b98a65e802972327e7fb74474ee8cfe5949c4
Andry Charlier <andry.charlier@studiohyperdrive.be>
2019-08-23 12:31:49 +0200

    feat: update toast service with Alert component
    Also add support for string and object as passed value

--- src/shared/services/toast-service.tsx
+++ src/shared/services/toast-service.tsx
@@ -1,8 +1,8 @@
-import React, { FC } from 'react';
-import { Slide, toast, ToastOptions } from 'react-toastify';
+import React, { FunctionComponent } from 'react';
+import { toast, ToastOptions } from 'react-toastify';
 
-// TODO: import alert from components when available
-// import { Alert } from '@viaa/avo2-components';
+import { Alert } from '@viaa/avo2-components';
+import { isNil, isString } from 'lodash-es';
 
 export enum TOAST_TYPE {
 	DANGER = 'danger',
@@ -11,17 +11,29 @@ export enum TOAST_TYPE {
 	SUCCESS = 'success',
 }
 
-// const Toast: FC<any> = ({ closeToast, message, type }) => (
-// 	<Alert close={closeToast} message={message} type={type} />
-// );
+interface ToastService {
+	m: string;
+	t?: TOAST_TYPE;
+	dark?: boolean;
+	options?: ToastOptions;
+}
 
-const toastOptions: ToastOptions = {
-	closeOnClick: false,
-	draggable: false,
-	transition: Slide,
-};
+const Toast: FunctionComponent<any> = ({ closeToast, ...rest }) => (
+	<Alert {...rest} className="u-spacer-top" close={closeToast} />
+);
 
-export default function toastService(message: string, type = TOAST_TYPE.INFO) {
-	// return toast(<Toast message={message} type={type} />, toastOptions);
-	return toast(message, toastOptions);
+export default function toastService(message: ToastService | string) {
+	const defaultType = TOAST_TYPE.INFO;
+
+	if (isNil(message)) {
+		return null;
+	}
+
+	if (isString(message)) {
+		return toast(<Toast message={message} type={defaultType} />);
+	}
+
+	const { dark, m, options = {}, t = defaultType } = message;
+
+	return toast(<Toast dark={dark} message={m} type={t} />, options);
 }
