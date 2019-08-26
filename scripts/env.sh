edfd16a9b335ecc4e4f313a717136a6afd99ce3d
Bert Verhelst <verhelstbert@gmail.com>
2019-08-26 16:00:05 +0200

    ci: disable secureRoute until idp is setup for int, qas and prd

--- scripts/env.sh
+++ scripts/env.sh
@@ -10,4 +10,4 @@ echo "window._ENV_ = {" >> ./env-config.js
 # TODO get a list of env variables from the global .env file, so we don't need to maintain these in both locations
 echo "  PROXY_URL: \"$PROXY_URL\"," >> ./env-config.js
 
-echo "}" >> ./env-config.js
+echo "};" >> ./env-config.js
