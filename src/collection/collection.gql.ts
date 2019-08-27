10529dc726efd1b4da60a04365169c1e4350b91f
Benjamin Naesen <benjamin.naesen@studiohyperdrive.be>
2019-08-27 14:37:25 +0200

    feat: retrieve item meta by external id

--- src/collection/collection.gql.ts
+++ src/collection/collection.gql.ts
@@ -191,3 +191,69 @@ export const GET_COLLECTIONS_BY_OWNER = gql`
 		}
 	}
 `;
+
+export const GET_ITEM_META_BY_EXTERNAL_ID = gql`
+	query getMetaItemByExternalId($externalId) {
+		app_item_meta(where: { external_id: { _eq: $externalID } }) {
+			external_id
+			bookmarks {
+				count
+				created_at
+				id
+				reference_id
+				updated_at
+			}
+			browse_path
+			created_at
+			depublish_at
+			description
+			duration
+			expiry_date
+			id
+			is_deleted
+			is_orphaned
+			is_published
+			issued
+			issued_edtf
+			item {
+				created_at
+				external_id
+				id
+				json
+				pid
+				updated_at
+			}
+			lom_classification
+			lom_context
+			lom_intendedenduserrole
+			lom_keywords
+			lom_languages
+			lom_typicalagerange
+			org_id
+			plays {
+				count
+				created_at
+				id
+				reference_id
+				updated_at
+			}
+			publish_at
+			series
+			thumbnail_path
+			title
+			type {
+				label
+				id
+			}
+			type_id
+			views {
+				count
+				created_at
+				id
+				reference_id
+				updated_at
+			}
+			updated_at
+		}
+	}
+`;
