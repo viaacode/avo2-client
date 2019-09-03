0e2bbae4bf64d08814455263820e6819a7c17f40
Bert Verhelst <verhelstbert@gmail.com>
2019-09-03 17:04:59 +0200

    fix: set moment locale to nl-be

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import moment from 'moment';
+
 import pkg from '../package.json';
 
 import { CustomWindow } from './shared/types/CustomWindow';
@@ -9,9 +11,15 @@ import '@viaa/avo2-components/dist/styles/main.css';
 
 import App from './App';
 
+const localization = require('moment/locale/nl-be');
+
+// Expose app info through the window object
 (window as CustomWindow).APP_INFO = {
 	mode: process.env.NODE_ENV,
 	version: pkg.version,
 };
+
+// Set moment language to dutch
+moment.locale('nl');
 
 ReactDOM.render(<App />, document.getElementById('root'));
