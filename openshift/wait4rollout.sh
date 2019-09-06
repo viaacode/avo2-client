cca9e5fdfe882428eb65a9e9cdc542cf74ba1cc7
violetina <schizoid69@gmail.com>
2019-09-06 17:21:52 +0200

    CICD: fix wait rollout int

--- openshift/wait4rollout.sh
+++ openshift/wait4rollout.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 ### ARGS: $1 env
-### script waits for next rollout 
+### script waits for next rollout
+set +x
 declare -i cur_ver
-declare -i b
+declare -i new_ver
 cur_ver=`oc rollout status deploymentconfig avo2-client-$1| awk '{print $3}'| sed 's/"//g'|rev|cut -d'-' -f1 |rev`
-new_ver=`(( $cur_ver + 1 ))`
-new_ver=cur_ver+1              
+#new_ver=`(( $cur_ver + 1 ))`
+new_ver=$cur_ver+1
+echo "xxxxxxxxx ${new_ver}"
 
-while [[ $cur_ver != $new_ver ]]; do
+while [ $cur_ver -ne $new_ver ]; do
    #echo wait
-   sleep 1
-   cur_ver=`oc rollout status deploymentconfig avo2-client-$1| awk '{print $3}'| sed 's/"//g'|rev|cut -d'-' -f1 |rev`
+   sleep 2
+   if  [ $cur_ver -eq $new_ver ] || exit 0;then echo;fi
+   cur_ver=$(oc rollout status deploymentconfig avo2-client-$1| awk '{print $3}'| sed 's/"//g'|rev|cut -d'-' -f1 |rev) 1>/dev/null
 done
