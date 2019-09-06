99adefbb15628f27fdea336e161cccf46675674f
violetina <schizoid69@gmail.com>
2019-09-06 21:38:52 +0200

    CICD: fix rollout init

--- openshift/wait4rollout.sh
+++ openshift/wait4rollout.sh
@@ -4,7 +4,8 @@
 set +x
 declare -i cur_ver
 declare -i new_ver
-cur_ver=`oc rollout status deploymentconfig avo2-client-$1| awk '{print $3}'| sed 's/"//g'|rev|cut -d'-' -f1 |rev` 2>/dev/null
+sleep 1
+cur_ver=`oc rollout status deploymentconfig avo2-client-$1| awk '{print $3}'| sed 's/"//g'|rev|cut -d'-' -f1 |rev` 
 new_ver="$((cur_ver + 1))"
 echo "${1} version to compare ${cur_ver}, new version ${new_ver}"
 
