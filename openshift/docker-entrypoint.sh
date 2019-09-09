2be05f2103ec57980b8bd68c34c237b928b058b8
violetina <schizoid69@gmail.com>
2019-09-09 09:55:13 +0200

    CICD: fix entrypoint, add bash option, remove npm opts

--- openshift/docker-entrypoint.sh
+++ openshift/docker-entrypoint.sh
@@ -3,11 +3,8 @@ WD=/usr/share/nginx/html
 cd $WD
 echo "setting env"
 sh ./env.sh
-if [[ "$1" -eq "int_test" ]]; then
-    
-    npm run integration-tests
-elif  [[ "$1" -eq "unit_test" ]]; then 
-    npm run unit-tests
-else  [[ "$1" -eq "bash" ]]; then 
+
+if  [[ "$1" -eq "bash" ]]; then 
     bash   
+else nginx -g 'daemon off;'
 fi
