646daeb8ec15608b59c8f48ae0d43a28032b5c97
violetina <schizoid69@gmail.com>
2019-09-09 14:29:36 +0200

    CICD: fix wait 4 rollout

--- openshift/wait4rollout.sh
+++ openshift/wait4rollout.sh
@@ -8,14 +8,13 @@ sleep 1
 cur_ver=`oc rollout status deploymentconfig avo2-client-$1| awk '{print $3}'| sed 's/"//g'|rev|cut -d'-' -f1 |rev` 
 new_ver="$((cur_ver + 1))"
 echo "${1} version to compare ${cur_ver}, new version ${new_ver}"
+oc rollout latest dc/avo2-client-${1} 
 
 
 while true ; do
-   sleep 3
-   [ -n "$cur_ver" ] && [ "${cur_ver}" -eq "${new_ver}" ]  2>/dev/null
+   oc rollout status deploymentconfig avo2-client-int  --revision=${new_ver} 
    if [ $? -ne 0 ]; then
-    echo version is not changed
-    cur_ver=`oc rollout status deploymentconfig avo2-client-$1 |tail -n1 | awk '{print $3}'| sed 's/"//g'|rev|cut -d'-' -f1 |rev` 2>/dev/null
+    sleep 4
     else echo "$1" ROLLED OUT && exit 0
    fi
 done
