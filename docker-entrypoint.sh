90f88dd0c13f33d2c8d413e5f85ee4f60eeb3e9e
violetina <schizoid69@gmail.com>
2019-09-09 09:42:51 +0200

    CICD: update init test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,12 +3,11 @@ WD=/usr/share/nginx/html
 cd $WD
 echo "setting env"
 sh ./env.sh
-if [[ "$1" -eq "int_test" ]]; then
-    
+if [[ "$1" -eq "int_test" ]]; then    
     npm run integration-tests
 elif  [[ "$1" -eq "unit_test" ]]; then 
-    npm run integration-tests
- elif  [[ "$1" -eq "bash" ]]; then 
+    npm run unit-tests
+elif  [[ "$1" -eq "bash" ]]; then 
     bash   
 else nginx -g 'daemon off;'
 fi
