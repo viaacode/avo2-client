73e75606cdb1bcb0776c6fa7bea6d496c3a1ef34
violetina <schizoid69@gmail.com>
2019-09-10 10:44:03 +0200

    CICD: try to fix tests

--- openshift/cy-openshift.sh
+++ openshift/cy-openshift.sh
@@ -4,8 +4,7 @@ docker run -i \
   -e NO_COLOR=1\
   -e PROXY_URL=http://avo2-proxy-qas-sc-avo2.apps.do-prd-okp-m0.do.viaa.be/ \
   -w /e2e \
+  -e ENV=int \
   --entrypoint sh \
-  cypress/included:3.4.0 -c "./scripts/env.sh &&\
-  PROXY_URL=http://avo2-proxy-qas-sc-avo2.apps.do-prd-okp-m0.do.viaa.be/ &&\
-  cypress run --project . \
-  --config baseUrl=http://avo2-client-int-sc-avo2.apps.do-prd-okp-m0.do.viaa.be/"
+  cypress/included:3.4.0 -c "CI=true npm ci --save-dev &&CI=true npm build . &&\
+   npm run integration-tests"
--- openshift/unittest-openshift.sh
+++ openshift/unittest-openshift.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+export PROXY_URL=http://avo2-proxy-qas-sc-avo2.apps.do-prd-okp-m0.do.viaa.be
+export ENV=int
 docker run -i \
   -v $PWD:/e2e \
   -w /e2e \
-  node:12-alpine npm install && sh ./scripts/env.sh && PROXY_URL=http://avo2-proxy-qas-sc-avo2.apps.do-prd-okp-m0.do.viaa.be/ CI=true npm run --no-watch unit-tests 
+  -e ENV=qas \
+  node:12-alpine sh -c "TZ=Europe/Brussels && apk add --no-cache tzdata && ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone &&\
+   sed -i \"6s/.*/uri: 'http:\/\/avo2-proxy-${ENV}-sc-avo2.apps.do-prd-okp-m0.do.viaa.be,'/\" src/shared/services/data-service.ts && CI=true npm ci --save-dev &&\
+   CI=true npm run --no-watch unit-tests" 
