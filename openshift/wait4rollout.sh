9d649af44cd50600357f166e4e5babeeb7302dce
violetina <schizoid69@gmail.com>
2019-09-11 15:13:47 +0200

    ci: fix rollout staging

--- openshift/wait4rollout.sh
+++ openshift/wait4rollout.sh
@@ -10,19 +10,19 @@ get_currentversion(){
    if echo $cur_ver | grep -Eq '^[+-]?[0-9]+$'
    then
       echo integer
+      new_ver="$((cur_ver + 1))" || exit 1
    else
       echo "!! got string  sleeping!"
       sleep 30
       cur_ver=`oc rollout status deploymentconfig avo2-client-${1}| awk '{print $3}'| sed 's/"//g'|rev|cut -d'-' -f1 |rev| tail -n1` 
-
+      new_ver="$((cur_ver + 1))" || exit 1
    fi
-new_ver="$((cur_ver + 1))" || exit 1
 }
 get_currentversion $1
 
 
 echo "${1} version to compare ${cur_ver}, new version ${new_ver}"
-is_ready=`oc get pod $(oc get pods | grep avo2-client-int | grep Running| awk '{print $1}' ) -o json | jq .status.containerStatuses[].ready`
+is_ready=`oc get pod $(oc get pods | grep avo2-client-${1}| grep Running| awk '{print $1}' ) -o json | jq .status.containerStatuses[].ready`
 oc rollout latest dc/avo2-client-${1} 
 
 while true ; do
@@ -33,7 +33,7 @@ while true ; do
       get_currentversion $1
    fi
 
-   oc rollout status deploymentconfig avo2-client-int --revision=${new_ver} 
+   oc rollout status deploymentconfig avo2-client-${1} --revision=${new_ver} 
    if [ $? -ne 0 ]; then
     sleep 4
     else echo "$1" ROLLED OUT && exit 0
