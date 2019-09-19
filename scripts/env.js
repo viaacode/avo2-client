bba1334a65e0bc839fa8f7bd9a38f3431c51988d
Benjamin Naesen <benjamin@naesen.be>
2019-09-19 18:28:28 +0200

    feat: convert env.sh to env.js script

---
+++ scripts/env.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+
+const CI_ENV_VARIABLES = {
+	PROXY_URL: process.env.PROXY_URL,
+	FLOW_PLAYER_TOKEN: process.env.FLOW_PLAYER_TOKEN,
+	FLOW_PLAYER_ID: process.env.FLOW_PLAYER_ID,
+}
+
+const envVariables = {};
+
+if (fs.existsSync(".env")) {
+	const envFile = fs.readFileSync(".env").toString();
+	const lines = envFile.split("\n").map((line) => line.trim());
+
+	lines.forEach((line) => {
+		const lineParts = line.split("=");
+
+		if (!CI_ENV_VARIABLES[lineParts[0]] || CI_ENV_VARIABLES[lineParts[0]][0] === "$") {
+			envVariables[lineParts[0]] = lineParts[1];
+		} else {
+			envVariables[lineParts[0]] = CI_ENV_VARIABLES[lineParts[0]];
+		}
+	})
+}
+
+let outputString = "window._ENV_ = {\n";
+
+Object.keys(envVariables).forEach((envName) => {
+	if (envName) {
+		outputString += `\"${envName}\": \"${envVariables[envName]}\",\n`;
+	}
+});
+
+outputString += "};";
+
+fs.writeFileSync("./env-config.js", outputString);
