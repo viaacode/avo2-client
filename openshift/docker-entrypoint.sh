413191a1b680f070f56a1664f9638137ce845848
Benjamin Naesen <benjamin@naesen.be>
2019-09-20 12:45:22 +0200

    feat: replace env.sh by env.js

--- openshift/docker-entrypoint.sh
+++ openshift/docker-entrypoint.sh
@@ -2,9 +2,9 @@
 WD=/usr/share/nginx/html
 cd $WD
 echo "setting env"
-sh ./env.sh
+node ./env.js
 
-if  [[ "$1" -eq "bash" ]]; then 
-    bash   
+if  [[ "$1" -eq "bash" ]]; then
+    bash
 else nginx -g 'daemon off;'
 fi
