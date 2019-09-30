124f50813fe36bd6d4c6f67691cbfeda6cdb95c0
Bert Verhelst <verhelstbert@gmail.com>
2019-09-30 16:44:52 +0200

    fix(login): avoid login loop when check-login call to api fails

--- src/404/views/NotFound.tsx
+++ src/404/views/NotFound.tsx
@@ -1,19 +1,23 @@
 import { Blankslate, Container } from '@viaa/avo2-components';
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, ReactNode } from 'react';
 
 interface NotFoundProps {
 	message?: string;
 	icon?: string;
+	children?: ReactNode;
 }
 
 export const NotFound: FunctionComponent<NotFoundProps> = ({
 	message = 'De pagina werd niet gevonden',
 	icon = 'search',
+	children = null,
 }) => {
 	return (
 		<Container mode="vertical" background="alt" className="o-container-vertical-title">
 			<Container size="medium" mode="horizontal">
-				<Blankslate body="" icon={icon} title={message} />
+				<Blankslate body="" icon={icon} title={message}>
+					{children}
+				</Blankslate>
 			</Container>
 		</Container>
 	);
