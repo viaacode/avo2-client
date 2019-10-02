50edee338e4e66a3fd745b99ee018e51866465b5
Benjamin Naesen <benjamin@naesen.be>
2019-10-02 10:40:25 +0200

    refactor: imports

--- src/404/views/NotFound.tsx
+++ src/404/views/NotFound.tsx
@@ -9,14 +9,12 @@ interface NotFoundProps {
 export const NotFound: FunctionComponent<NotFoundProps> = ({
 	message = 'De pagina werd niet gevonden',
 	icon = 'search',
-}) => {
-	return (
-		<Container mode="vertical" background="alt" className="o-container-vertical-title">
-			<Container size="medium" mode="horizontal">
-				<Blankslate body="" icon={icon} title={message} />
-			</Container>
+}) => (
+	<Container mode="vertical" background="alt" className="o-container-vertical-title">
+		<Container size="medium" mode="horizontal">
+			<Blankslate body="" icon={icon} title={message} />
 		</Container>
-	);
-};
+	</Container>
+);
 
 export default NotFound;
