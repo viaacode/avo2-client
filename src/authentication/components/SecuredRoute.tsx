517bc7510973c20cedf32741140dba0bbb692989
Bert Verhelst <verhelstbert@gmail.com>
2019-10-04 16:33:03 +0200

    fix(login): auto redirect to desired route if already logged into saml

--- src/authentication/components/SecuredRoute.tsx
+++ src/authentication/components/SecuredRoute.tsx
@@ -1,9 +1,12 @@
-import React, { ComponentType, FunctionComponent } from 'react';
+import React, { ComponentType, FunctionComponent, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Redirect, Route, RouteComponentProps, withRouter } from 'react-router';
 
+import { Spinner } from '@viaa/avo2-components';
+import { Dispatch } from 'redux';
 import { RouteParts } from '../../constants';
-import { selectLogin } from '../store/selectors';
+import { getLoginState } from '../store/actions';
+import { selectLogin, selectLoginError, selectLoginLoading } from '../store/selectors';
 import { LoginResponse } from '../store/types';
 
 export interface SecuredRouteProps {
@@ -11,10 +14,37 @@ export interface SecuredRouteProps {
 	path?: string;
 	exact?: boolean;
 	loginState: LoginResponse | null;
+	loginStateLoading: boolean;
+	loginStateError: boolean;
+	getLoginState: () => Dispatch;
 }
 
-const SecuredRoute: FunctionComponent<SecuredRouteProps & RouteComponentProps> = props => {
-	const { component, path, exact, loginState } = props;
+const SecuredRoute: FunctionComponent<SecuredRouteProps & RouteComponentProps> = ({
+	history,
+	component,
+	path,
+	exact,
+	loginState,
+	loginStateLoading,
+	loginStateError,
+	getLoginState,
+}) => {
+	useEffect(() => {
+		if (!loginState && !loginStateLoading && !loginStateError) {
+			getLoginState();
+		}
+	}, [getLoginState, loginState, loginStateLoading, loginStateError]);
+
+	if (!loginState || loginStateLoading) {
+		// Wait for login check
+		return (
+			<div
+				style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '400px' }}
+			>
+				<Spinner size="large" />
+			</div>
+		);
+	}
 
 	return (
 		<Route
@@ -26,6 +56,8 @@ const SecuredRoute: FunctionComponent<SecuredRouteProps & RouteComponentProps> =
 					const Component = component;
 					return <Component />;
 				}
+
+				// On errors or not logged in => redirect to login or register page
 				return (
 					<Redirect
 						to={{
@@ -41,6 +73,19 @@ const SecuredRoute: FunctionComponent<SecuredRouteProps & RouteComponentProps> =
 
 const mapStateToProps = (state: any) => ({
 	loginState: selectLogin(state),
+	loginStateLoading: selectLoginLoading(state),
+	loginStateError: selectLoginError(state),
 });
 
-export default withRouter(connect(mapStateToProps)(SecuredRoute));
+const mapDispatchToProps = (dispatch: Dispatch) => {
+	return {
+		getLoginState: () => dispatch(getLoginState() as any),
+	};
+};
+
+export default withRouter(
+	connect(
+		mapStateToProps,
+		mapDispatchToProps
+	)(SecuredRoute)
+);
