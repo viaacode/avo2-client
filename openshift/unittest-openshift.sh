bba918a0fc3538bad46e9706220a8010aa49a990
violetina <schizoid69@gmail.com>
2019-10-07 17:02:17 +0200

    ci: fix unit test, no need for local dir mount

--- openshift/unittest-openshift.sh
+++ openshift/unittest-openshift.sh
@@ -3,23 +3,31 @@ ENV=int
 PROXY_URL=http://avo2-proxy-${ENV}-sc-avo2.apps.do-prd-okp-m0.do.viaa.be
 TAG=`git describe --tags`
 
+
+
 build(){
   echo "*** Building image ${TAG} ***"
   docker build --target build  --build-arg CI=false --build-arg NODE_ENV=ci --build-arg PRODUCTION=false -t avo2-client-${TAG}:ci .
 }
-run(){
-  docker run -t \
-  -v $PWD:/e2e \
-  -w /e2e \
-  -e ENV=int \
-  avo2-client-${TAG}:ci sh -c "CI=true npm ci --production=false --save-dev && npm run build && npm run --no-watch unit-tests"
-  }
 
+utest(){
+  docker run -i avo2-client-${TAG}:ci  sh -c "CI=true npm ci --production=false --save-dev && npm run build && npm run --no-watch unit-tests"
 
-build
+}
 echo "*** running the image ***"
-run 
-
+check(){
+docker images | head | grep avo2-client-`git describe --tags` | grep ci
+}
+check
+if [ $? -eq 0  ]
+then
+	echo "____________ CI Image found not building __________________"
+	utest
+else
+	echo "____________ CI Image NOT found Starting build ____________"
+	build
+	utest
+fi
 if [ $? -ne 0 ];then
     echo "########## For now we return 0 BUT TEST IS Failed, results will be saved to jenkins  ############"
     exit 0
