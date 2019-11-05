b7d9d1bfeac731ff778c4969a68e6864190e5e46
Benjamin Naesen <benjamin@naesen.be>
2019-11-05 20:34:40 +0100

    feat(collection): added empty state on collections overview

--- src/collection/views/CollectionOverview.tsx
+++ src/collection/views/CollectionOverview.tsx
@@ -14,12 +14,15 @@ import {
 	MetaData,
 	MetaDataItem,
 	Pagination,
+	Spacer,
 	Table,
 	Thumbnail,
 } from '@viaa/avo2-components';
 import { Avo } from '@viaa/avo2-types';
 import { compact } from 'lodash-es';
 
+import NotFound from '../../404/views/NotFound';
+import { getProfileId } from '../../authentication/helpers/get-profile-info';
 import { RouteParts } from '../../constants';
 import { ITEMS_PER_PAGE } from '../../my-workspace/constants';
 import { DataQueryComponent } from '../../shared/components/DataComponent/DataQueryComponent';
@@ -31,7 +34,6 @@ import toastService, { TOAST_TYPE } from '../../shared/services/toast-service';
 import { IconName } from '../../shared/types/types';
 import { DELETE_COLLECTION, GET_COLLECTIONS_BY_OWNER } from '../graphql';
 
-import { getProfileId } from '../../authentication/helpers/get-profile-info';
 import './CollectionOverview.scss';
 
 interface CollectionsProps extends RouteComponentProps {
@@ -175,43 +177,70 @@ const Collections: FunctionComponent<CollectionsProps> = ({ numberOfCollections,
 		}
 	};
 
+	const onClickCreate = () => history.push(`/${RouteParts.Search}`);
+
 	const renderCollections = (
 		collections: Avo.Collection.Collection[],
 		refetchCollections: () => void
-	) => (
-		<>
-			<Table
-				columns={[
-					{ id: 'thumbnail', label: '', col: '2' },
-					{ id: 'title', label: 'Titel', col: '6', sortable: true },
-					{ id: 'updated_at', label: 'Laatst bewerkt', col: '3', sortable: true },
-					{ id: 'inFolder', label: 'In map', col: '2' },
-					{ id: 'access', label: 'Toegang', col: '2' },
-					{ id: 'actions', label: '', col: '1' },
-				]}
-				data={collections}
-				emptyStateMessage="Geen resultaten gevonden"
-				renderCell={renderCell}
-				rowKey="id"
-				variant="styled"
-			/>
-			<Pagination
-				pageCount={Math.ceil(numberOfCollections / ITEMS_PER_PAGE)}
-				currentPage={page}
-				onPageChange={setPage}
-			/>
+	) => {
+		const renderTable = () => (
+			<>
+				<Table
+					columns={[
+						{ id: 'thumbnail', label: '', col: '2' },
+						{ id: 'title', label: 'Titel', col: '6', sortable: true },
+						{ id: 'updated_at', label: 'Laatst bewerkt', col: '3', sortable: true },
+						{ id: 'inFolder', label: 'In map', col: '2' },
+						{ id: 'access', label: 'Toegang', col: '2' },
+						{ id: 'actions', label: '', col: '1' },
+					]}
+					data={collections}
+					emptyStateMessage="Geen resultaten gevonden"
+					renderCell={renderCell}
+					rowKey="id"
+					variant="styled"
+				/>
+				<Pagination
+					pageCount={Math.ceil(numberOfCollections / ITEMS_PER_PAGE)}
+					currentPage={page}
+					onPageChange={setPage}
+				/>
+			</>
+		);
 
-			<DeleteObjectModal
-				title="Verwijder collectie?"
-				body="Bent u zeker, deze actie kan niet worden ongedaan gemaakt"
-				isOpen={isDeleteModalOpen}
-				onClose={() => setIsDeleteModalOpen(false)}
-				deleteObjectCallback={() => deleteCollection(refetchCollections)}
-			/>
-		</>
-	);
+		const renderEmptyFallback = () => (
+			<NotFound icon="collection" message="Je hebt nog geen collecties aangemaakt.">
+				<p>
+					Een collectie is een verzameling van video- of audiofragmenten rond een bepaald thema of
+					voor een bepaalde les. Nadat je een collectie hebt aangemaakt kan je deze delen met andere
+					gebruikers om samen aan te werken. Andere gebruikers kunnen ook collecties met jou delen
+					die je dan hier terugvindt.
+				</p>
+				<Spacer margin="top">
+					<Button
+						type="primary"
+						icon="add"
+						label="Maak je eerste collectie"
+						onClick={onClickCreate}
+					/>
+				</Spacer>
+			</NotFound>
+		);
 
-	// TODO get actual owner id from ldap user + map to old drupal userid
+		return (
+			<>
+				{collections.length ? renderTable() : renderEmptyFallback()}
+				<DeleteObjectModal
+					title="Verwijder collectie?"
+					body="Bent u zeker, deze actie kan niet worden ongedaan gemaakt"
+					isOpen={isDeleteModalOpen}
+					onClose={() => setIsDeleteModalOpen(false)}
+					deleteObjectCallback={() => deleteCollection(refetchCollections)}
+				/>
+			</>
+		);
+	};
+
 	return (
 		<DataQueryComponent
 			query={GET_COLLECTIONS_BY_OWNER}
