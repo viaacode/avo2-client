c46cf7b43880133255aa1a8b24f278d0fa723f29
Bert Verhelst <verhelstbert@gmail.com>
2019-11-07 12:12:29 +0100

    feat(error-view): make not found more general as error-view

--- src/404/routes.tsx
+++
@@ -1,8 +0,0 @@
-import React, { ReactNode } from 'react';
-import { Route } from 'react-router';
-
-import NotFound from './views/NotFound';
-
-export const renderNotFoundRoutes = (): ReactNode[] => [
-	<Route component={NotFound} key="notfound" />,
-];
