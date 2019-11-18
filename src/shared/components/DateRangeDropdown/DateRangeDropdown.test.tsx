a7003f9aac9d307d3e1e63a2cf75924031a6c20c
Andry Charlier <andry.charlier@studiohyperdrive.be>
2019-11-18 10:59:22 +0100

    test: fix imports on broken tests

--- src/shared/components/DateRangeDropdown/DateRangeDropdown.test.tsx
+++ src/shared/components/DateRangeDropdown/DateRangeDropdown.test.tsx
@@ -2,7 +2,7 @@ import { mount, shallow } from 'enzyme';
 import React from 'react';
 
 import { action } from '@storybook/addon-actions';
-import { DateRangeDropdown } from './DateRangeDropdown';
+import DateRangeDropdown from './DateRangeDropdown';
 
 describe('<Checkbox />', () => {
 	it('Should be able to render', () => {
