27b4f434d9db7378fac25e1289d31b1abcb46a69
Andry Charlier <andry.charlier@studiohyperdrive.be>
2019-11-29 17:12:08 +0100

    feat(admin-cb): add ColorSelect component

---
+++ src/admin/content-block/components/ColorSelect/ColorSelect.tsx
@@ -0,0 +1,39 @@
+import classnames from 'classnames';
+import React, { FunctionComponent } from 'react';
+import Select from 'react-select';
+import { Props } from 'react-select/src/Select';
+
+import { Flex, Spacer } from '@viaa/avo2-components';
+
+import { ReactSelectOption } from '../../../../shared/types';
+
+import './ColorSelect.scss';
+
+interface ColorSelectProps extends Props {}
+
+const ColorSelect: FunctionComponent<ColorSelectProps> = ({
+	className,
+	noOptionsMessage = ({ inputValue }) => `Geen kleuren gevonden: ${inputValue}`,
+	placeholder = '',
+	...rest
+}) => {
+	const renderLabel = ({ label, value }: ReactSelectOption<string>) => (
+		<Flex>
+			<span className={`c-color-select__preview u-${value}`} />
+			<Spacer margin="left">{label}</Spacer>
+		</Flex>
+	);
+
+	return (
+		<Select
+			className={classnames(className, 'c-color-select')}
+			noOptionsMessage={noOptionsMessage}
+			placeholder={placeholder}
+			{...rest}
+			classNamePrefix="react-select"
+			formatOptionLabel={renderLabel}
+		/>
+	);
+};
+
+export default ColorSelect;
