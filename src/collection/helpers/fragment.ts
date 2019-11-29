e432df1e156026df5d4988e2cb4e49980a3a2152
Benjamin Naesen <benjamin@naesen.be>
2019-11-29 12:54:04 +0100

    fix: remove console.log

--- src/collection/helpers/fragment.ts
+++ src/collection/helpers/fragment.ts
@@ -21,13 +21,6 @@ export const getFragmentProperty = (
 		: get(itemMetaData, prop, '');
 };
 
-/*
-[
-	FRAGMENT A,
-	FRAGMENT B,
-]
-*/
-
 const getFragmentByPosition = (fragments: Avo.Collection.Fragment[], position: number) => {
 	const fragmentAtPosition = fragments.find(
 		(fragment: Avo.Collection.Fragment) => fragment.position === position
@@ -49,22 +42,10 @@ export const swapFragmentsPositions = (
 	const currentFragment = getFragmentByPosition(fragments, currentFragmentId);
 	const otherFragment = getFragmentByPosition(fragments, currentFragmentId - delta);
 
-	console.log('FRAGMENTS', fragments);
-
-	console.log('OLD CURRENT FRAGMENT', currentFragment);
-	console.log('OLD OTHER FRAGMENT', otherFragment);
-
 	if (currentFragment && otherFragment) {
-		console.log('delta', delta);
-
 		currentFragment.position -= delta;
 		otherFragment.position += delta;
-
-		console.log('NEW CURRENT FRAGMENT', currentFragment);
-		console.log('NEW OTHER FRAGMENT', otherFragment);
 	}
-
-	console.log('NEW FRAGMENTS', fragments);
 
 	return fragments;
 };
