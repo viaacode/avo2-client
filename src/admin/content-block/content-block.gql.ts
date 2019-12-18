2d1f509264b96dc510e456d6075a404b29629355
Andry Charlier <andry.charlier@studiohyperdrive.be>
2019-12-18 16:08:21 +0100

    feat(admin-cb): update gql mutations

--- src/admin/content-block/content-block.gql.ts
+++ src/admin/content-block/content-block.gql.ts
@@ -13,19 +13,23 @@ export const GET_CONTENT_BLOCKS_BY_CONTENT_ID = gql`
 `;
 
 export const INSERT_CONTENT_BLOCKS = gql`
-	mutation insertContentBlocks(
-		$contentId: Int!
-		$contentBlocks: [app_content_blocks_insert_input!]!
-	) {
+	mutation insertContentBlocks($contentBlocks: [app_content_blocks_insert_input!]!) {
 		affected_rows
 	}
 `;
 
-export const UPDATE_CONTENT_BLOCKS = gql`
-	mutation insertContentBlocks(
-		$contentId: Int!
-		$contentBlocks: [app_content_blocks_insert_input!]!
-	) {
-		affected_rows
+export const UPDATE_CONTENT_BLOCK = gql`
+	mutation updateContentBlock($id: Int!, $contentBlock: app_content_blocks_set_input!) {
+		update_app_content_blocks(where: { id: { _eq: $id } }, _set: $contentBlock) {
+			affected_rows
+		}
+	}
+`;
+
+export const DELETE_CONTENT_BLOCK = gql`
+	mutation deleteContentBlock($id: Int!) {
+		delete_app_content_blocks(where: { id: { _eq: $id } }) {
+			affected_rows
+		}
 	}
 `;
