70279529ee7a513835aa26c62b686300b656ea59
Andry Charlier <andry.charlier@studiohyperdrive.be>
2020-01-17 12:25:16 +0100

    fix(admin): layout styling

--- src/admin/shared/layouts/AdminLayout/AdminLayout.tsx
+++ src/admin/shared/layouts/AdminLayout/AdminLayout.tsx
@@ -1,9 +1,12 @@
+import classnames from 'classnames';
 import React, { FunctionComponent, ReactNode } from 'react';
 
 import { Container, useSlot } from '@viaa/avo2-components';
 
 import { ActionsBar, TopBar } from '../../components';
 import { AdminLayoutActions, AdminLayoutBody, AdminLayoutHeader } from './AdminLayout.slots';
+
+import './AdminLayout.scss';
 
 interface AdminLayoutProps {
 	children?: ReactNode;
@@ -23,20 +26,20 @@ const AdminLayout: FunctionComponent<AdminLayoutProps> = ({
 	const header = useSlot(AdminLayoutHeader, children);
 
 	return (
-		<>
+		<div className={classnames({ 'l-admin--with-actions': !!actions })}>
 			<TopBar navigateBack={navigateBack} />
 			{header}
 			{(pageTitle || (children && !body)) && (
 				<Container className={className} mode="vertical" size="small">
 					<Container mode="horizontal">
-						{pageTitle && <h1 className="c-h2 u-m-t-0">{pageTitle}</h1>}
+						{pageTitle && <h1 className="c-h2 u-m-0">{pageTitle}</h1>}
 						{!body && children}
 					</Container>
 				</Container>
 			)}
 			{body}
 			{actions && <ActionsBar fixed>{actions}</ActionsBar>}
-		</>
+		</div>
 	);
 };
 
