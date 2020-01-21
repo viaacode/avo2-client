c4faf0e7f34afc0456451dc7fb9f8bcbebde87e0
Bert Verhelst <verhelstbert@gmail.com>
2020-01-21 11:42:25 +0100

    fix(tslint): fix imports from index file in same folder

--- src/collection/views/CollectionEdit.tsx
+++ src/collection/views/CollectionEdit.tsx
@@ -27,6 +27,7 @@ import { Avo } from '@viaa/avo2-types';
 
 import { DefaultSecureRouteProps } from '../../authentication/components/SecuredRoute';
 import { getProfileName } from '../../authentication/helpers/get-profile-info';
+import { selectUser } from '../../authentication/store/selectors';
 import {
 	ControlledDropdown,
 	DataQueryComponent,
@@ -37,14 +38,9 @@ import { createDropdownMenuItem, navigate, renderAvatar } from '../../shared/hel
 import { ApolloCacheManager } from '../../shared/services/data-service';
 import { trackEvents } from '../../shared/services/event-logging-service';
 import toastService from '../../shared/services/toast-service';
+import { AppState } from '../../store';
 import { COLLECTIONS_ID, WORKSPACE_PATH } from '../../workspace/workspace.const';
 
-import { redirectToClientPage } from '../../authentication/helpers/redirects';
-import { selectUser } from '../../authentication/store/selectors';
-import { APP_PATH } from '../../constants';
-import { AppState } from '../../store';
-
-import { CollectionEditContent, CollectionEditMetaData } from '.';
 import { COLLECTION_EDIT_TABS } from '../collection.const';
 import {
 	DELETE_COLLECTION,
@@ -61,6 +57,8 @@ import {
 	ShareCollectionModal,
 } from '../components';
 import { swapFragmentsPositions } from '../helpers';
+import CollectionEditContent from './CollectionEditContent';
+import CollectionEditMetaData from './CollectionEditMetaData';
 
 interface CollectionEditProps extends DefaultSecureRouteProps<{ id: string }> {}
 
