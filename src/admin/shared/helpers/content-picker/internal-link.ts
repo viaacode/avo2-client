c5ca0614b1ba9277ebb22972de963fc02b973316
Bert Verhelst <verhelstbert@gmail.com>
2020-02-17 18:04:31 +0100

    fix(content-picker): add more static routes

--- src/admin/shared/helpers/content-picker/internal-link.ts
+++ src/admin/shared/helpers/content-picker/internal-link.ts
@@ -1,20 +1,31 @@
+import { remove } from 'lodash-es';
+
 import { APP_PATH } from '../../../../constants';
 
 import { PickerSelectItemGroup } from '../../types';
 import { parsePickerItem } from './parse-picker';
 
-const APP_PATH_ARRAY = Object.entries(APP_PATH);
+const APP_PATH_ARRAY = Object.values(APP_PATH);
 
 // Return InternalLinkItems items from APP_PATH
 export const fetchInternalLinks = async (limit: number): Promise<PickerSelectItemGroup> =>
 	parseInternalLinks(APP_PATH_ARRAY, limit);
 
-export const parseInternalLinks = (raw: any, limit: number) => {
-	const filteredItems = raw.slice(0, limit).filter((item: any) => !item[1].includes(':'));
+export const parseInternalLinks = (allPaths: string[], limit: number) => {
+	const paths = allPaths.slice(0, limit).filter((path: any) => !path.includes(':'));
+	remove(paths, (item: any) => item[0] === 'ASSIGNMENT_CREATE');
+	paths.push('/mijn-werkruimte/collecties');
+	paths.push('/mijn-werkruimte/bundels');
+	paths.push('/mijn-werkruimte/opdrachten');
+	paths.push('/mijn-werkruimte/bladwijzers');
+	paths.push('/instellingen/profiel');
+	paths.push('/instellingen/account');
+	paths.push('/instellingen/email');
+	paths.push('/instellingen/notificaties');
 
-	const parsedInternalLinkItemsItems = filteredItems.map((item: any) => ({
-		label: item[1],
-		value: parsePickerItem('INTERNAL_LINK', item[1]),
+	const parsedInternalLinkItemsItems = paths.sort().map((path: string) => ({
+		label: path,
+		value: parsePickerItem('INTERNAL_LINK', path),
 	}));
 
 	return {
