3fa49ae8d2099196531efddcae3b8bbe4bc19e95
Benjamin Naesen <benjamin@naesen.be>
2020-02-28 14:46:29 +0100

    feat(content-picker): stringify output

--- src/admin/shared/helpers/content-picker/parse-picker.ts
+++ src/admin/shared/helpers/content-picker/parse-picker.ts
@@ -1,29 +1,35 @@
+import queryString from 'query-string';
+
+import { toastService } from '../../../../shared/services';
+import i18n from '../../../../shared/translations/i18n';
+
 import { ContentPickerType, PickerItem } from '../../types';
 
-const parseSearchQuery = (input: string) => {
-	// replace %22 by "
-	const replacedString = input.split('%22').join('"');
+const parseSearchQuery = (input: string): string => {
+	try {
+		// replace %22 by "
+		const replacedString = decodeURI(input);
 
-	// split on first instance of ?
-	const splitString = replacedString.includes('?')
-		? replacedString
-				.split('?')
-				.slice(1)
-				.join('?')
-		: replacedString;
+		// split on first instance of ?
+		const splitString = replacedString.includes('?')
+			? replacedString
+					.split('?')
+					.slice(1)
+					.join('?')
+			: replacedString;
 
-	// parse as objects
-	const output = splitString.split('&').reduce((acc: any, curr: any) => {
-		const prop = curr.split('=');
-		const key = decodeURIComponent(prop[0]);
-		const value = decodeURIComponent(prop[1]);
+		// parse as objects
 
-		acc[key] = value[0] === '{' || value[0] === '[' ? JSON.parse(value) : value;
+		const output = queryString.parse(splitString);
+		output.filters = JSON.parse(output.filters as string);
 
-		return acc;
-	}, {});
+		return JSON.stringify(output);
+	} catch (err) {
+		console.error('Failed to parse search query input', err);
+		toastService.danger(i18n.t('Gelieve een correcte zoekfilter-link in te vullen.'), false);
 
-	return output;
+		return 'Ongeldige zoekfilter';
+	}
 };
 
 export const parsePickerItem = (type: ContentPickerType, value: string): PickerItem => ({
