47d5a531103a18f2b5d9b736c61b6a31140749b8
Benjamin Naesen <benjamin@naesen.be>
2020-03-02 11:11:18 +0100

    feat(cb-media-player): retrieve and apply still

--- src/admin/content-block/helpers/wrappers/BlockMediaPlayerWrapper.tsx
+++ src/admin/content-block/helpers/wrappers/BlockMediaPlayerWrapper.tsx
@@ -1,11 +1,12 @@
 import { get } from 'lodash-es';
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
 import { BlockFlowPlayer, ButtonAction } from '@viaa/avo2-components';
 
 import { getEnv } from '../../../../shared/helpers';
 import { ToastService } from '../../../../shared/services';
 import { fetchPlayerTicket } from '../../../../shared/services/player-ticket-service';
+import { getVideoStills } from '../../../../shared/services/stills-service';
 import i18n from '../../../../shared/translations/i18n';
 
 interface MediaPlayerProps {
@@ -15,32 +16,49 @@ interface MediaPlayerProps {
 
 export const MediaPlayer: FC<MediaPlayerProps> = ({ item, title }) => {
 	const [playerTicket, setPlayerTicket] = useState<string>();
+	const [videoStill, setVideoStill] = useState<string>();
 
-	const initFlowPlayer = () =>
-		!playerTicket &&
-		fetchPlayerTicket(item.value.toString())
-			.then((data: string) => {
-				setPlayerTicket(data);
-			})
-			.catch((err: any) => {
-				console.error(err);
-				ToastService.danger(
-					i18n.t(
-						'admin/content-block/helpers/wrappers/block-media-player-wrapper___het-ophalen-van-het-player-ticket-is-mislukt'
-					)
-				);
-			});
+	useEffect(() => {
+		retrieveStill();
+	});
+
+	const initFlowPlayer = () => {
+		if (!playerTicket) {
+			fetchPlayerTicket(item.value.toString())
+				.then((data: string) => {
+					setPlayerTicket(data);
+				})
+				.catch((err: any) => {
+					console.error(err);
+					ToastService.danger(
+						i18n.t(
+							'admin/content-block/helpers/wrappers/block-media-player-wrapper___het-ophalen-van-het-player-ticket-is-mislukt'
+						)
+					);
+				});
+		}
+	};
+
+	const retrieveStill = async () => {
+		const videoStills = await getVideoStills([
+			{ externalId: item.value.toString(), startTime: 0 },
+		]);
+
+		setVideoStill(videoStills[0].previewImagePath);
+	};
 
 	return (
 		<div className="c-video-player t-player-skin--dark">
-			<BlockFlowPlayer
-				title={title}
-				src={playerTicket ? playerTicket.toString() : null}
-				poster="https://via.placeholder.com/1920x1080"
-				onInit={get(item, 'value') ? initFlowPlayer : () => {}}
-				token={getEnv('FLOW_PLAYER_TOKEN')}
-				dataPlayerId={getEnv('FLOW_PLAYER_ID')}
-			/>
+			{!!videoStill && (
+				<BlockFlowPlayer
+					title={title}
+					src={playerTicket ? playerTicket.toString() : null}
+					poster={videoStill}
+					onInit={get(item, 'value') ? initFlowPlayer : () => {}}
+					token={getEnv('FLOW_PLAYER_TOKEN')}
+					dataPlayerId={getEnv('FLOW_PLAYER_ID')}
+				/>
+			)}
 		</div>
 	);
 };
