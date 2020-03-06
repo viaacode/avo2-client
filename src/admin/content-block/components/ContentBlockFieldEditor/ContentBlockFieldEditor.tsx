cfbb89e1507082f5be1d8194d2370fda934034ba
Benjamin Naesen <benjamin@naesen.be>
2020-03-06 13:20:16 +0100

    feat(content-picker): use fieldKey for initialValues

--- src/admin/content-block/components/ContentBlockFieldEditor/ContentBlockFieldEditor.tsx
+++ src/admin/content-block/components/ContentBlockFieldEditor/ContentBlockFieldEditor.tsx
@@ -58,7 +58,7 @@ export const ContentBlockFieldEditor: FunctionComponent<ContentBlockFieldProps> 
 				onSelect: (picked: PickerItem) => {
 					handleChange(type, fieldKey, { value: picked }, stateIndex);
 				},
-				initialValues: get(state as any, 'buttonAction'),
+				initialValues: get(state as any, fieldKey),
 			};
 			break;
 		case ContentBlockEditor.IconPicker:
