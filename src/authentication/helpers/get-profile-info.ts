c48343ef19661af2ce5990ba64abdf813cac0c36
Bert Verhelst <verhelstbert@gmail.com>
2020-03-20 10:36:26 +0100

    fix(profile): move comment about only teachers having to fill profile

--- src/authentication/helpers/get-profile-info.ts
+++ src/authentication/helpers/get-profile-info.ts
@@ -92,11 +92,11 @@ export function getProfileStamboekNumber(user: Avo.User.User | undefined): strin
 export function isProfileComplete(user: Avo.User.User): boolean {
 	const profile = get(user, 'profile');
 
+	// Only teachers have to fill in their profile for now
 	if (user.role_id !== 2) {
 		return true;
 	}
 
-	// Only teachers have to fill in their profile for now
 	return (
 		!!profile &&
 		!!profile.organizations &&
