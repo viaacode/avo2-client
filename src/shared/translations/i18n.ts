01909b228835d99c1f5dbb5067b376d6818e0e5c
Benjamin Naesen <benjamin@naesen.be>
2020-03-25 15:30:10 +0100

    fix(translations): xhr

--- src/shared/translations/i18n.ts
+++ src/shared/translations/i18n.ts
@@ -1,17 +1,48 @@
 import i18n from 'i18next';
+import Fetch from 'i18next-fetch-backend';
+// import XHR from 'i18next-xhr-backend';
 import { get } from 'lodash-es';
 import { initReactI18next } from 'react-i18next';
 
-import translations from './nl.json';
+import { getEnv } from '../helpers';
 
-i18n.use(initReactI18next) // passes i18n down to react-i18next
+// import translations from './nl.json';
+
+// {
+//     "name": "translations-frontend",
+//     "value": {
+//         "shared/components/share-through-email-modal/share-through-email-modal___stuur-een-link-via-email": "Stuur een link via emails"
+//     }
+// }
+
+// const retrieveFrontendTranslations = async () => {
+// 	const url = `${getEnv('PROXY_URL')}/translations/nl.json`;
+
+// 	const response = await fetch(url, {
+// 		method: 'GET',
+// 		headers: {
+// 			'Content-Type': 'application/json',
+// 		},
+// 	});
+
+// 	const translations = await response.json();
+
+// 	return translations;
+// };
+
+i18n.use(Fetch)
+	.use(initReactI18next) // passes i18n down to react-i18next
 	.init({
-		debug: get(window, '_ENV_.ENV') === 'local',
-		resources: {
-			nl: {
-				translation: translations,
-			},
+		resources: {},
+		backend: {
+			loadPath: `${getEnv('PROXY_URL')}/translations/nl.json`,
+			parse: (data: any) => ({
+				nl: {
+					translations: JSON.parse(data).value,
+				},
+			}),
 		},
+		debug: get(window, '_ENV_.ENV') === 'local',
 		keySeparator: '^',
 		nsSeparator: '^',
 		lng: 'nl',
@@ -20,5 +51,10 @@ i18n.use(initReactI18next) // passes i18n down to react-i18next
 			escapeValue: false,
 		},
 	});
+
+// retrieveFrontendTranslations().then(translations => {
+// 	debugger;
+// 	i18n.addResources('nl', 'translation', get(translations, 'value'));
+// });
 
 export default i18n;
