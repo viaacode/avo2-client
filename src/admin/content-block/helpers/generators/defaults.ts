dec8b23d1e52a5e637212f70657b9b2d9c164a07
Bert Verhelst <verhelstbert@gmail.com>
2020-05-05 09:35:55 +0200

    fix(block-preview): block padding handles padding, not block content

--- src/admin/content-block/helpers/generators/defaults.ts
+++ src/admin/content-block/helpers/generators/defaults.ts
@@ -25,7 +25,6 @@ export const BLOCK_STATE_DEFAULTS = (
 	position: number,
 	backgroundColor: Color = Color.White,
 	headerBackgroundColor: Color = Color.Transparent,
-	headerHeight: string = '0', // Currently we only need 2 block background colors for the PageOverviewBlock component
 	padding: PaddingFieldState = {
 		top: 'top',
 		bottom: 'bottom',
@@ -36,7 +35,6 @@ export const BLOCK_STATE_DEFAULTS = (
 	position,
 	backgroundColor,
 	headerBackgroundColor,
-	headerHeight,
 	padding,
 	userGroupIds,
 });
