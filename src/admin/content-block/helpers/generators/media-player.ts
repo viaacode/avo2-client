22b92b73af511168e45cdeb72a4e3675ebb8a9b6
Bert Verhelst <verhelstbert@gmail.com>
2020-05-19 16:28:55 +0200

    feat(admin-sidebar): add quick links to go to start pages

--- src/admin/content-block/helpers/generators/media-player.ts
+++ src/admin/content-block/helpers/generators/media-player.ts
@@ -47,11 +47,11 @@ export const MEDIA_PLAYER_BLOCK_CONFIG = (position: number = 0): ContentBlockCon
 				}
 			),
 			item: VIDEO_FIELD(undefined, {
-				validator: () => [],
+				validator: undefined,
 			}),
 			src: FILE_FIELD(undefined, {
 				label: i18n.t('Eigen video uploaden (optioneel)'),
-				validator: () => [],
+				validator: undefined,
 				editorProps: {
 					allowMulti: false,
 					allowedTypes: VIDEO_TYPES,
@@ -61,7 +61,7 @@ export const MEDIA_PLAYER_BLOCK_CONFIG = (position: number = 0): ContentBlockCon
 			}),
 			poster: FILE_FIELD(undefined, {
 				label: i18n.t('Eigen poster uploaden (optioneel)'),
-				validator: () => [],
+				validator: undefined,
 				editorProps: {
 					allowMulti: false,
 					allowedTypes: PHOTO_TYPES,
--- src/admin/shared/helpers/omitByDeep.ts
+++ src/admin/shared/helpers/omitByDeep.ts
@@ -1,10 +1,10 @@
-import { forIn, isArray, isObject } from 'lodash-es';
+import { forIn, isArray, isPlainObject } from 'lodash-es';
 
 export function omitByDeep(obj: any, predicate: (key: string, value: any) => boolean): any {
 	const returnObj: any = isArray(obj) ? [] : {};
 	forIn(obj, (value, key) => {
 		if (!predicate(key, value)) {
-			if (isObject(value)) {
+			if (isPlainObject(value)) {
 				// Recursively omit inner object properties
 				returnObj[key] = omitByDeep(value, predicate);
 			} else {
