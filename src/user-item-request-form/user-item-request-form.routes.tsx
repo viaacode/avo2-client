66484030315f945c098ae2a6acf0400454c8d091
Bert Verhelst <verhelstbert@gmail.com>
2020-05-29 12:54:20 +0200

    feat(user-item-request-form): layout fixes + confirm on separate page

--- src/user-item-request-form/user-item-request-form.routes.tsx
+++ src/user-item-request-form/user-item-request-form.routes.tsx
@@ -4,6 +4,7 @@ import { SecuredRoute } from '../authentication/components';
 import { APP_PATH } from '../constants';
 
 import UserItemRequestForm from './views/UserItemRequestForm';
+import UserItemRequestFormConfirm from './views/UserItemRequestFormConfirm';
 
 export const renderUserItemRequestFormRoutes = (): ReactNode[] => [
 	<SecuredRoute
@@ -11,5 +12,11 @@ export const renderUserItemRequestFormRoutes = (): ReactNode[] => [
 		exact
 		path={APP_PATH.USER_ITEM_REQUEST_FORM.route}
 		key={APP_PATH.USER_ITEM_REQUEST_FORM.route}
+	/>,
+	<SecuredRoute
+		component={UserItemRequestFormConfirm}
+		exact
+		path={APP_PATH.USER_ITEM_REQUEST_FORM_CONFIRM.route}
+		key={APP_PATH.USER_ITEM_REQUEST_FORM_CONFIRM.route}
 	/>,
 ];
