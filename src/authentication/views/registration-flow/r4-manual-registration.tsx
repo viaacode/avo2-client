fdd6911ed07969a3acc35a189c475ff49cea8036
Benjamin Naesen <benjamin.naesen@studiohyperdrive.be>
2020-06-19 13:40:27 +0200

    fix(manual-registration): remove temp lit

--- src/authentication/views/registration-flow/r4-manual-registration.tsx
+++ src/authentication/views/registration-flow/r4-manual-registration.tsx
@@ -136,7 +136,7 @@ const ManualRegistration: FunctionComponent<ManualRegistrationProps> = ({ histor
 			}
 
 			const parsedEducationLevels = selectedEducationLevels
-				.map(selectedEducationLevel => `${selectedEducationLevel.label}`)
+				.map(selectedEducationLevel => selectedEducationLevel.label)
 				.join(', ');
 
 			// create zendesk ticket
