973c3c6e3552ea9714944d6983bff259b9b3f587
Bert Verhelst <verhelstbert@gmail.com>
2020-08-28 10:37:26 +0200

    fix(dynamic-route-resolver): update sitemap url to include extension

--- src/dynamic-route-resolver/dynamic-route-resolver.const.ts
+++ src/dynamic-route-resolver/dynamic-route-resolver.const.ts
@@ -30,5 +30,5 @@ export const GET_REDIRECTS: () => { [avo1Path: string]: string } = () => ({
 		'/nieuws/barend-van-heusden-aan-het-woord-over-cultuur-de-spiegel',
 
 	'/klaar.json': `${getEnv('PROXY_URL')}/klaar/klaar.json`,
-	'/sitemap.xml': `${getEnv('PROXY_URL')}/sitemap`,
+	'/sitemap.xml': `${getEnv('PROXY_URL')}/sitemap.xml`,
 });
