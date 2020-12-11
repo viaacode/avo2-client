cf3fbe9e46dde2fef7fdb56f844faaa24024c3d9
Bert Verhelst <verhelstbert@gmail.com>
2020-12-11 11:23:02 +0100

    feat(user-service): link delete users ui to backend route

--- src/admin/users/user.types.ts
+++ src/admin/users/user.types.ts
@@ -61,12 +61,26 @@ export interface RawPermission {
 
 export type UserBulkAction = 'block' | 'unblock' | 'delete' | 'change_subjects' | 'export';
 
+// TODO use types from typings library after update to v2.27.0
 export type UserDeleteOption =
 	| 'DELETE_PRIVATE_KEEP_NAME'
 	| 'TRANSFER_PUBLIC'
 	| 'TRANSFER_ALL'
 	| 'ANONYMIZE_PUBLIC'
 	| 'DELETE_ALL';
+
+// TODO use types from typings library after update to v2.27.0
+export interface BulkDeleteUsersBody {
+	profileIds: string[];
+	deleteOption: UserDeleteOption;
+	transferToProfileId?: string;
+}
+
+// TODO use types from typings library after update to v2.27.0
+export interface BulkBlockUsersBody {
+	profileIds: string[];
+	isBlocked: boolean;
+}
 
 export interface DeleteContentCounts {
 	publicCollections: number;
