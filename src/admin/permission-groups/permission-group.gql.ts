b2658fe3d58e65304dc87567ce9f417484e9d2d7
Benjamin Naesen <benjamin@naesen.be>
2021-04-08 19:51:37 +0200

    fix(permission-group): name

--- src/admin/permission-groups/permission-group.gql.ts
+++ src/admin/permission-groups/permission-group.gql.ts
@@ -1,7 +1,7 @@
 import { gql } from 'apollo-boost';
 
 export const GET_PERMISSION_GROUPS = gql`
-	query getAllPermissionGroups(
+	query getPermissionGroups(
 		$where: users_permission_groups_bool_exp!
 		$offset: Int!
 		$limit: Int!
