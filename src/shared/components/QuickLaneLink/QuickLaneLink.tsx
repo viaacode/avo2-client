eed3c6346a7ba5cc36ae48c4b390b899a4ba68e7
Ian Emsens <ian.emsens@studiohyperdrive.be>
2021-11-26 09:46:16 +0100

    feat: add new mutator to QuickLaneLink

--- src/shared/components/QuickLaneLink/QuickLaneLink.tsx
+++ src/shared/components/QuickLaneLink/QuickLaneLink.tsx
@@ -1,19 +1,28 @@
+import classNames from 'classnames';
 import React, { FunctionComponent } from 'react';
 
 import { generateQuickLaneHref } from '../../helpers/generate-quick-lane-href';
 
+import './QuickLaneLink.scss';
+
 interface QuickLaneLinkProps {
 	id: string;
 	label?: string;
+	short?: boolean;
 }
 
 const defaultLabel = (id: string) => {
 	return `${window.location.origin}${generateQuickLaneHref(id)}`;
 };
 
-const QuickLaneLink: FunctionComponent<QuickLaneLinkProps> = ({ id, label }) => {
+const QuickLaneLink: FunctionComponent<QuickLaneLinkProps> = ({ id, label, short }) => {
+	const className = classNames({
+		'c-quick-lane-link': true,
+		'c-quick-lane-link--short': short === true,
+	});
+
 	return (
-		<a target="_blank" href={generateQuickLaneHref(id)}>
+		<a className={className} target="_blank" href={generateQuickLaneHref(id)}>
 			{label ? label : defaultLabel(id)}
 		</a>
 	);
