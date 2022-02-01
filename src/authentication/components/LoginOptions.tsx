f39452f92c9c33bb00ae9945072f1cd2f98f8512
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-02-01 15:33:05 +0100

    feat(avo-1829, avo-1832): adjust existing login flow

--- src/authentication/components/LoginOptions.tsx
+++ src/authentication/components/LoginOptions.tsx
@@ -1,10 +1,14 @@
 import React, { FunctionComponent } from 'react';
-import { Trans, useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import { RouteComponentProps } from 'react-router';
 
-import { Button, Spacer } from '@viaa/avo2-components';
+import { Button, Icon, Spacer, Tabs } from '@viaa/avo2-components';
 
+import { APP_PATH } from '../../constants';
+import { ROUTE_PARTS } from '../../shared/constants';
+import { useTabs } from '../../shared/hooks';
 import {
+	redirectToClientPage,
 	redirectToServerACMIDMLogin,
 	redirectToServerItsmeLogin,
 	redirectToServerKlascementLogin,
@@ -18,86 +22,225 @@ export interface LoginOptionsProps extends RouteComponentProps {
 	onOptionClicked?: () => void;
 }
 
+const LoginOptionsTabs = {
+	TEACHER: 'lesgever',
+	STUDENT: 'leerling',
+};
+
 const LoginOptions: FunctionComponent<LoginOptionsProps> = ({
+	history,
 	location,
 	onOptionClicked = () => {},
 }) => {
 	const [t] = useTranslation();
 
-	return (
-		<div className="m-login-options">
-			<Spacer margin="bottom-large">
-				<Spacer margin="top-small">
+	const [tab, setActiveTab, tabs] = useTabs(
+		[
+			{
+				label: t('Lesgever'),
+				id: LoginOptionsTabs.TEACHER,
+				icon: 'user-teacher',
+			},
+			{
+				label: t('Leerling'),
+				id: LoginOptionsTabs.STUDENT,
+				icon: 'user-student',
+			},
+		],
+		LoginOptionsTabs.TEACHER
+	);
+
+	const renderTitle = () => {
+		switch (tab) {
+			case LoginOptionsTabs.TEACHER:
+				return (
+					<>
+						<Icon name="user-teacher"></Icon>
+						{t('Log in als lesgever')}
+					</>
+				);
+
+			case LoginOptionsTabs.STUDENT:
+				return (
+					<>
+						<Icon name="user-student"></Icon>
+						{t('Log in als leerling')}
+					</>
+				);
+
+			default:
+				break;
+		}
+	};
+
+	const getButtons = () => {
+		switch (tab) {
+			case LoginOptionsTabs.TEACHER:
+				return [
 					<Button
 						block
 						label={t(
 							'authentication/components/login-options___inloggen-met-e-mailadres'
 						)}
+						icon="mail"
 						type="primary"
-						className="c-login-with-archief"
+						className="c-login-with-archief c-button-mail"
 						onClick={() => {
 							onOptionClicked();
 							redirectToServerLoginPage(location);
 						}}
+					/>,
+
+					<Button
+						block
+						type="secondary"
+						className="c-button-itsme"
+						icon="itsme"
+						iconType="multicolor"
+						label={t('authentication/components/login-options___itsme')}
+						onClick={() => {
+							onOptionClicked();
+							redirectToServerItsmeLogin(location);
+						}}
+					/>,
+
+					<Button
+						block
+						type="secondary"
+						className="c-button-acmidm"
+						icon="eid"
+						label={t(
+							'authentication/components/login-options___e-id-of-een-digitale-sleutel'
+						)}
+						onClick={() => {
+							onOptionClicked();
+							redirectToServerACMIDMLogin(location);
+						}}
+					/>,
+
+					<Button
+						block
+						className="c-button-smartschool"
+						icon="smartschool"
+						label={t(
+							'authentication/components/login-options___inloggen-met-smartschool'
+						)}
+						onClick={() => {
+							onOptionClicked();
+							redirectToServerSmartschoolLogin(location);
+						}}
+					/>,
+
+					<Button
+						block
+						className="c-button-klascement"
+						icon="klascement"
+						label={t(
+							'authentication/components/login-options___inloggen-met-klas-cement'
+						)}
+						onClick={() => {
+							onOptionClicked();
+							redirectToServerKlascementLogin(location);
+						}}
+					/>,
+				];
+
+			case LoginOptionsTabs.STUDENT:
+				return [
+					<Button
+						block
+						label={t(
+							'authentication/components/login-options___inloggen-met-e-mailadres'
+						)}
+						icon="mail"
+						type="primary"
+						className="c-login-with-archief c-button-mail"
+						onClick={() => {
+							onOptionClicked();
+							redirectToServerLoginPage(location);
+						}}
+					/>,
+
+					<Button
+						block
+						className="c-button-smartschool"
+						icon="smartschool"
+						label={t(
+							'authentication/components/login-options___inloggen-met-smartschool'
+						)}
+						onClick={() => {
+							onOptionClicked();
+							redirectToServerSmartschoolLogin(location);
+						}}
+					/>,
+				];
+
+			default:
+				break;
+		}
+	};
+
+	const renderFallbackButton = () => {
+		switch (tab) {
+			case LoginOptionsTabs.TEACHER:
+				return (
+					<Button
+						block
+						label={t(
+							'authentication/views/register-or-login___account-aanmaken-als-lesgever'
+						)}
+						type="primary"
+						onClick={() => {
+							onOptionClicked();
+							redirectToClientPage(APP_PATH.STAMBOEK.route, history);
+						}}
 					/>
+				);
+
+			case LoginOptionsTabs.STUDENT:
+				return (
+					<Button
+						block
+						label={t(
+							'authentication/views/register-or-login___krijg-toegang-als-leerling'
+						)}
+						type="primary"
+						onClick={() => {
+							onOptionClicked();
+							redirectToClientPage(`/${ROUTE_PARTS.pupils}`, history);
+						}}
+					/>
+				);
+
+			default:
+				break;
+		}
+	};
+
+	return (
+		<div className="m-login-options">
+			<Tabs tabs={tabs} onClick={setActiveTab} />
+
+			<div className="m-login-options__wrapper">
+				<Spacer margin={['bottom-large']}>
+					<h2 className="c-h3 u-m-0 m-login-options__title">{renderTitle()}</h2>
 				</Spacer>
-			</Spacer>
-			<p>
-				<Trans i18nKey="authentication/components/login-options___of-kies-voor">
-					Of kies voor ...
-				</Trans>
-			</p>
-			<Spacer margin={['top-small', 'bottom-small']}>
-				<Button
-					block
-					type="secondary"
-					className="c-button-itsme"
-					icon="itsme"
-					iconType="multicolor"
-					label={t('authentication/components/login-options___itsme')}
-					onClick={() => {
-						onOptionClicked();
-						redirectToServerItsmeLogin(location);
-					}}
-				/>
-			</Spacer>
-			<Spacer margin={['top-small', 'bottom-small']}>
-				<Button
-					block
-					type="secondary"
-					className="c-button-acmidm"
-					icon="eid"
-					label={t(
-						'authentication/components/login-options___e-id-of-een-digitale-sleutel'
-					)}
-					onClick={() => {
-						onOptionClicked();
-						redirectToServerACMIDMLogin(location);
-					}}
-				/>
-			</Spacer>
-			<Spacer margin={['top-small', 'bottom-small']}>
-				<Button
-					block
-					className="c-button-smartschool"
-					icon="smartschool"
-					label={t('authentication/components/login-options___inloggen-met-smartschool')}
-					onClick={() => {
-						onOptionClicked();
-						redirectToServerSmartschoolLogin(location);
-					}}
-				/>
-			</Spacer>
-			<Button
-				block
-				className="c-button-klascement"
-				icon="klascement"
-				label={t('authentication/components/login-options___inloggen-met-klas-cement')}
-				onClick={() => {
-					onOptionClicked();
-					redirectToServerKlascementLogin(location);
-				}}
-			/>
+
+				{getButtons()?.map((button) => (
+					<Spacer
+						key={`button--${button.props.className}`}
+						margin={['top-small', 'bottom-small']}
+					>
+						{button}
+					</Spacer>
+				))}
+
+				<hr />
+
+				<h3 className="c-h4 u-m-0">{t('Nog geen account?')}</h3>
+
+				<Spacer margin={['top-small']}>{renderFallbackButton()}</Spacer>
+			</div>
 		</div>
 	);
 };
--- src/authentication/views/RegisterOrLogin.tsx
+++ src/authentication/views/RegisterOrLogin.tsx
@@ -4,8 +4,6 @@ import MetaTags from 'react-meta-tags';
 import { RouteComponentProps, withRouter } from 'react-router';
 
 import {
-	BlockHeading,
-	Button,
 	Column,
 	Container,
 	Flex,
@@ -16,10 +14,8 @@ import {
 	Spacer,
 } from '@viaa/avo2-components';
 
-import { APP_PATH, GENERATE_SITE_TITLE } from '../../constants';
-import { ROUTE_PARTS } from '../../shared/constants';
+import { GENERATE_SITE_TITLE } from '../../constants';
 import LoginOptions from '../components/LoginOptions';
-import { redirectToClientPage } from '../helpers/redirects';
 
 import './RegisterOrLogin.scss';
 
@@ -50,76 +46,31 @@ const RegisterOrLogin: FunctionComponent<RegisterOrLoginProps & RouteComponentPr
 						)}
 					/>
 				</MetaTags>
+
 				<Modal className="c-register-login-view__modal" isOpen size="medium">
 					<ModalBody>
-						<Grid>
+						<Grid className="u-bg-gray-100">
 							<Column size="3-6">
-								<Spacer margin="bottom-extra-large">
-									<Flex center orientation="horizontal">
-										<FlexItem>
-											<h2 className="c-h2 u-m-0">
+								<Flex className="u-maximize-height" center orientation="vertical">
+									<FlexItem className="c-register-login-view__text">
+										<h2 className="c-h2 u-m-0">
+											{t(
+												'authentication/views/register-or-login___welkom-op-het-archief-voor-onderwijs'
+											)}
+										</h2>
+
+										<Spacer margin={['top-small']}>
+											<p>
 												{t(
-													'authentication/views/register-or-login___welkom-op-het-archief-voor-onderwijs'
+													'authentication/views/register-or-login___maak-een-gratis-account-aan-en-verrijk-je-lessen-met-beeld-en-geluid-op-maat-van-de-klas'
 												)}
-											</h2>
-											<Spacer margin={['top-small', 'bottom']}>
-												<p>
-													{t(
-														'authentication/views/register-or-login___maak-een-gratis-account-aan-en-verrijk-je-lessen-met-beeld-en-geluid-op-maat-van-de-klas'
-													)}
-												</p>
-											</Spacer>
-											<Spacer margin={['top-small', 'bottom-small']}>
-												<Button
-													block
-													label={t(
-														'authentication/views/register-or-login___account-aanmaken-als-lesgever'
-													)}
-													type="primary"
-													onClick={() =>
-														redirectToClientPage(
-															APP_PATH.STAMBOEK.route,
-															history
-														)
-													}
-												/>
-											</Spacer>
-											<Button
-												block
-												label={t(
-													'authentication/views/register-or-login___krijg-toegang-als-leerling'
-												)}
-												type="primary"
-												onClick={() =>
-													redirectToClientPage(
-														`/${ROUTE_PARTS.pupils}`,
-														history
-													)
-												}
-											/>
-										</FlexItem>
-									</Flex>
-								</Spacer>
-							</Column>
-							<Column size="3-6">
-								<Flex center orientation="horizontal">
-									<FlexItem>
-										<BlockHeading type="h2" className="u-m-0">
-											{t(
-												'authentication/views/register-or-login___reeds-een-account'
-											)}
-											<br />
-											{t(
-												'authentication/views/register-or-login___log-dan-hier-in'
-											)}
-										</BlockHeading>
-										<LoginOptions
-											history={history}
-											location={location}
-											match={match}
-										/>
+											</p>
+										</Spacer>
 									</FlexItem>
 								</Flex>
+							</Column>
+							<Column size="3-6" className="u-bg-white">
+								<LoginOptions history={history} location={location} match={match} />
 							</Column>
 						</Grid>
 					</ModalBody>
