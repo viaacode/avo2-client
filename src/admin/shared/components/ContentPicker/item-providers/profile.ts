d14f9875a9a6bc395c48fe7fb2cf7cada537f534
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-05-09 10:21:11 +0200

    fix(avo-1866): check which user wants to fetch others

--- src/admin/shared/components/ContentPicker/item-providers/profile.ts
+++ src/admin/shared/components/ContentPicker/item-providers/profile.ts
@@ -1,31 +1,53 @@
 import { Avo } from '@viaa/avo2-types';
+import { UserSchema } from '@viaa/avo2-types/types/user';
 
+import {
+	PermissionName,
+	PermissionService,
+} from '../../../../../authentication/helpers/permission-service';
 import { CustomError } from '../../../../../shared/helpers';
 import { UserService } from '../../../../users/user.service';
 import { PickerSelectItem } from '../../../types';
 import { parsePickerItem } from '../helpers/parse-picker';
 
+const shouldFetchUsersInCompany = (user?: UserSchema) => {
+	return (
+		user &&
+		PermissionService.hasPerm(user, PermissionName.VIEW_USERS_IN_SAME_COMPANY) &&
+		!PermissionService.hasPerm(user, PermissionName.EDIT_ANY_USER)
+	);
+};
+
 // Fetch profiles from GQL
 export const retrieveProfiles = async (
 	name: string | null,
-	limit: number = 5
+	limit: number = 5,
+	user?: UserSchema
 ): Promise<PickerSelectItem[]> => {
 	try {
-		const response: [Avo.User.Profile[], number] = await UserService.getProfiles(
-			0,
-			'last_access_at',
-			'desc',
-			'dateTime',
-			!!name
+		const where = {
+			...(!!name
 				? {
 						_or: [
 							{ full_name: { _ilike: `%${name}%` } },
 							{ mail: { _ilike: `%${name}%` } },
 						],
 				  }
-				: undefined,
+				: {}),
+			...(shouldFetchUsersInCompany(user)
+				? { company_id: { _eq: user?.profile?.company_id } }
+				: {}),
+		};
+
+		const response: [Avo.User.Profile[], number] = await UserService.getProfiles(
+			0,
+			'last_access_at',
+			'desc',
+			'dateTime',
+			where,
 			limit
 		);
+
 		return parseProfiles(response[0]);
 	} catch (err) {
 		throw new CustomError('Failed to get profiles for content picker', err, { name, limit });
