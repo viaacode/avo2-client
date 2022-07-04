725b32db16a91de850cc5aa3c4fd26705bb3fd9e
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2022-07-04 14:53:07 +0200

    fix(avo-1910): remove console.log

--- src/shared/hooks/scroll-to-id.ts
+++ src/shared/hooks/scroll-to-id.ts
@@ -3,7 +3,6 @@ import { useCallback, useEffect, useState } from 'react';
 export function useScrollToId(id: string | null): void {
 	const [timerId, setTimerId] = useState<number | null>(null);
 	const scrollDownToFocusedItem = useCallback(() => {
-		console.log('checking id is present');
 		if (id) {
 			const item = document.getElementById(id);
 			if (item && item.offsetTop) {
