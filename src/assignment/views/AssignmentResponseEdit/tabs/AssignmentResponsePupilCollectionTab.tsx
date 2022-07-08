6625dbcfa2d6e90b04b5c89b8e36cb4155af41e2
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2022-07-08 11:59:05 +0200

    fix(avo-1912): only render empty state when no blocks, push saveBar down

--- src/assignment/views/AssignmentResponseEdit/tabs/AssignmentResponsePupilCollectionTab.tsx
+++ src/assignment/views/AssignmentResponseEdit/tabs/AssignmentResponsePupilCollectionTab.tsx
@@ -198,34 +198,36 @@ const AssignmentResponsePupilCollectionTab: FunctionComponent<
 						},
 					}}
 				/>
-				<EmptyStateMessage
-					imgSrc={emptyCollectionPlaceholder}
-					imgAlt={t(
-						'assignment/views/assignment-response-edit/tabs/assignment-response-pupil-collection-tab___lege-collectie-placeholder-afbeelding'
-					)}
-					title={t(
-						'assignment/views/assignment-response-edit/tabs/assignment-response-pupil-collection-tab___mijn-collectie-is-nog-leeg'
-					)}
-					message={
-						<>
-							{t(
-								'assignment/views/assignment-response-edit/tabs/assignment-response-pupil-collection-tab___ga-naar'
-							)}{' '}
-							<Button
-								type="inline-link"
-								label={t(
-									'assignment/views/assignment-response-edit/tabs/assignment-response-pupil-collection-tab___zoeken'
+				{!assignmentResponse?.pupil_collection_blocks?.length && (
+					<EmptyStateMessage
+						imgSrc={emptyCollectionPlaceholder}
+						imgAlt={t(
+							'assignment/views/assignment-response-edit/tabs/assignment-response-pupil-collection-tab___lege-collectie-placeholder-afbeelding'
+						)}
+						title={t(
+							'assignment/views/assignment-response-edit/tabs/assignment-response-pupil-collection-tab___mijn-collectie-is-nog-leeg'
+						)}
+						message={
+							<>
+								{t(
+									'assignment/views/assignment-response-edit/tabs/assignment-response-pupil-collection-tab___ga-naar'
+								)}{' '}
+								<Button
+									type="inline-link"
+									label={t(
+										'assignment/views/assignment-response-edit/tabs/assignment-response-pupil-collection-tab___zoeken'
+									)}
+									onClick={() =>
+										setTab(ASSIGNMENT_RESPONSE_CREATE_UPDATE_TABS.SEARCH)
+									}
+								/>{' '}
+								{t(
+									'assignment/views/assignment-response-edit/tabs/assignment-response-pupil-collection-tab___om-fragmenten-toe-te-voegen-of-druk-op-de-plus-knop-hierboven-als-je-tekstblokken-wil-aanmaken'
 								)}
-								onClick={() =>
-									setTab(ASSIGNMENT_RESPONSE_CREATE_UPDATE_TABS.SEARCH)
-								}
-							/>{' '}
-							{t(
-								'assignment/views/assignment-response-edit/tabs/assignment-response-pupil-collection-tab___om-fragmenten-toe-te-voegen-of-druk-op-de-plus-knop-hierboven-als-je-tekstblokken-wil-aanmaken'
-							)}
-						</>
-					}
-				/>
+							</>
+						}
+					/>
+				)}
 			</Container>
 			{renderedModals}
 		</Container>
