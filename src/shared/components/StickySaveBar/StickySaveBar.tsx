7b7e022d6df97dc7fdea9ac9defe4c3d30c25465
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2022-07-08 15:08:53 +0200

    fix(avo-1912): rework sticky save bar to use pure css for mobile

--- src/shared/components/StickySaveBar/StickySaveBar.tsx
+++ src/shared/components/StickySaveBar/StickySaveBar.tsx
@@ -1,9 +1,8 @@
-import { Button, Flex, FlexItem, Spacer, StickyEdgeBar } from '@viaa/avo2-components';
+import { Button, StickyEdgeBar } from '@viaa/avo2-components';
 import React, { FunctionComponent } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import './StickySaveBar.scss';
-import { isMobileWidth } from '../../helpers';
 
 export interface StickySaveBarProps {
 	isVisible: boolean;
@@ -23,30 +22,13 @@ export const StickySaveBar: FunctionComponent<StickySaveBarProps> = ({
 	}
 	return (
 		<StickyEdgeBar className="c-sticky-save-bar">
-			<Flex orientation={isMobileWidth() ? 'vertical' : 'horizontal'}>
-				<FlexItem className="c-sticky-save-bar--description">
-					<p>
-						<strong>
-							{t('assignment/views/assignment-edit___wijzigingen-opslaan')}
-						</strong>
-					</p>
-				</FlexItem>
-
-				<FlexItem shrink>
-					{isMobileWidth() && <Spacer margin="top" />}
-					<Flex>
-						<Button
-							label={t('assignment/views/assignment-edit___annuleer')}
-							onClick={onCancel}
-						/>
-						<Button
-							type="tertiary"
-							label={t('assignment/views/assignment-edit___opslaan')}
-							onClick={onSave}
-						/>
-					</Flex>
-				</FlexItem>
-			</Flex>
+			<strong>{t('assignment/views/assignment-edit___wijzigingen-opslaan')}</strong>
+			<Button label={t('assignment/views/assignment-edit___annuleer')} onClick={onCancel} />
+			<Button
+				type="tertiary"
+				label={t('assignment/views/assignment-edit___opslaan')}
+				onClick={onSave}
+			/>
 		</StickyEdgeBar>
 	);
 };
