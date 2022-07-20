397745c7247db0953704a08898ce98e8a4414070
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2022-07-20 17:09:05 +0200

    fix(avo-2040): show spinner during assignment loading
    https://meemoo.atlassian.net/browse/AVO-2040

--- src/assignment/views/AssignmentEdit.tsx
+++ src/assignment/views/AssignmentEdit.tsx
@@ -582,15 +582,13 @@ const AssignmentEdit: FunctionComponent<DefaultSecureRouteProps<{ id: string }>>
 
 	const renderPageContent = () => {
 		if (assignmentLoading) {
-			if (!assignment) {
-				return (
-					<Spacer margin="top-extra-large">
-						<Flex orientation="horizontal" center>
-							<Spinner size="large" />
-						</Flex>
-					</Spacer>
-				);
-			}
+			return (
+				<Spacer margin="top-extra-large">
+					<Flex orientation="horizontal" center>
+						<Spinner size="large" />
+					</Flex>
+				</Spacer>
+			);
 		}
 		if (assignmentError) {
 			return <ErrorView {...assignmentError} />;
