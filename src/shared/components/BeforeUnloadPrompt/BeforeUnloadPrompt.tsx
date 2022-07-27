79fd6f0f70f800ec98bf5de1e5c16953f230c77c
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-07-27 14:50:49 +0200

    fix(avo-2078): prevent unneeded warnings

--- src/shared/components/BeforeUnloadPrompt/BeforeUnloadPrompt.tsx
+++ src/shared/components/BeforeUnloadPrompt/BeforeUnloadPrompt.tsx
@@ -1,22 +1,26 @@
 import React, { FunctionComponent } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Prompt } from 'react-router-dom';
+import { Prompt, useLocation } from 'react-router-dom';
 
 export const BeforeUnloadPrompt: FunctionComponent<{ when: boolean; message?: string }> = ({
 	when,
 	message,
 }) => {
 	const [t] = useTranslation();
+	const current = useLocation();
 
 	return (
 		<Prompt
 			when={when}
-			message={
-				message ||
-				t(
-					'shared/components/before-unload-prompt/before-unload-prompt___er-zijn-nog-niet-opgeslagen-wijzigingen-weet-u-zeker-dat-u-de-pagina-wil-verlaten'
-				)
-			}
+			message={(next) => {
+				return (
+					current.pathname === next.pathname ||
+					message ||
+					t(
+						'shared/components/before-unload-prompt/before-unload-prompt___er-zijn-nog-niet-opgeslagen-wijzigingen-weet-u-zeker-dat-u-de-pagina-wil-verlaten'
+					)
+				);
+			}}
 		/>
 	);
 };
