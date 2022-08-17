cb246cd2845fac1adc1487184deee7a450ba361d
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2022-08-17 15:56:20 +0200

    fix(avo-2114): avoid collections that are null in bookmarks
    https://meemoo.atlassian.net/browse/AVO-2114
    
    I'm guessing the collection was removed and the bookmark was not?
    We'll need to investigate this further in the future.

--- src/assignment/views/AssignmentEdit.tsx
+++ src/assignment/views/AssignmentEdit.tsx
@@ -442,7 +442,7 @@ const AssignmentEdit: FunctionComponent<DefaultSecureRouteProps<{ id: string }>>
 
 	const renderTabs = useMemo(() => <Tabs tabs={tabs} onClick={onTabClick} />, [tabs, onTabClick]);
 
-	const renderTabContent = useMemo(() => {
+	const renderedTabContent = useMemo(() => {
 		switch (tab) {
 			case ASSIGNMENT_CREATE_UPDATE_TABS.Inhoud:
 				if (pastDeadline) {
@@ -547,7 +547,9 @@ const AssignmentEdit: FunctionComponent<DefaultSecureRouteProps<{ id: string }>>
 						</Spacer>
 					)}
 
-					<Spacer margin={['top-large', 'bottom-extra-large']}>{renderTabContent}</Spacer>
+					<Spacer margin={['top-large', 'bottom-extra-large']}>
+						{renderedTabContent}
+					</Spacer>
 
 					{renderedModals}
 					{draggableListModal}
