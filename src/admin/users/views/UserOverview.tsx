50c4dfba3bf650942ee6660707ebbba576d93838
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2022-10-03 15:29:45 +0200

    fix(avo-2129): filter using blocked_at, fetch using last_blocked_at

--- src/admin/users/views/UserOverview.tsx
+++ src/admin/users/views/UserOverview.tsx
@@ -162,10 +162,8 @@ const UserOverview: FunctionComponent<UserOverviewProps & RouteComponentProps & 
 				...getDateRangeFilters(
 					filters,
 					['blocked_at', 'unblocked_at'],
-					[
-						'last_blocked_at.aggregate.max.created_at',
-						'last_unblocked_at.aggregate.max.created_at',
-					]
+					// Filtering has to happen on blocked_at, fetching on last_blocked_at: https://meemoo.atlassian.net/browse/AVO-2129?focusedCommentId=34844
+					['blocked_at.max', 'unblocked_at.max']
 				)
 			);
 
