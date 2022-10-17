5f34148f3b41856d07ceb2e1835e021cfe652dd0
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2022-10-17 13:10:57 +0200

    fix(avo-2209): fix ts file import in generated db types file

--- src/shared/generated/graphql-db-types.ts
+++ src/shared/generated/graphql-db-types.ts
@@ -1,5 +1,6 @@
+// @ts-nocheck
 import { useMutation, useQuery, UseMutationOptions, UseQueryOptions } from '@tanstack/react-query';
-import { fetchData } from './src/shared/services/data-service.ts';
+import { fetchData } from '../services/data-service.ts';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
