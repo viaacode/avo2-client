d030fd274c69560c8776c6a7e87501a2f7477f3f
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-11-07 16:23:51 +0100

    feat(avo-2217): integrate unrelease components

--- src/shared/helpers/get-subtitles.ts
+++ src/shared/helpers/get-subtitles.ts
@@ -1,4 +1,4 @@
-import { FlowplayerTrack } from '@viaa/avo2-components';
+import { FlowplayerTrackSchema } from '@meemoo/react-components';
 import { Avo } from '@viaa/avo2-types';
 import { get } from 'lodash-es';
 
@@ -8,17 +8,19 @@ import { getEnv } from './env';
 
 export function getSubtitles(
 	item: Avo.Item.Item | undefined | null
-): FlowplayerTrack[] | undefined {
+): FlowplayerTrackSchema[] | undefined {
 	const collaterals = get(item, 'item_collaterals') || [];
 	if (!collaterals.length) {
 		return undefined;
 	}
-	return collaterals.map((collateral: Avo.Item.Subtitle, index: number): FlowplayerTrack => {
-		return {
-			id: collateral.external_id,
-			default: index === 0,
-			src: `${getEnv('PROXY_URL')}/subtitles/convert-srt-to-vtt${collateral.path}`,
-			label: i18n.t('shared/helpers/get-subtitles___nederlands') + (index + 1),
-		};
-	});
+	return collaterals.map(
+		(collateral: Avo.Item.Subtitle, index: number): FlowplayerTrackSchema => {
+			return {
+				id: collateral.external_id,
+				default: index === 0,
+				src: `${getEnv('PROXY_URL')}/subtitles/convert-srt-to-vtt${collateral.path}`,
+				label: i18n.t('shared/helpers/get-subtitles___nederlands') + (index + 1),
+			};
+		}
+	);
 }
