fd1f5a6f231b45002c20449068cfffb9cea9ba60
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-11-09 13:39:26 +0100

    feat(avo-2199): add create button

--- src/admin/content-page/views/ContentPageOverviewPage.tsx
+++ src/admin/content-page/views/ContentPageOverviewPage.tsx
@@ -1,14 +1,28 @@
 import { ContentPageOverview } from '@meemoo/admin-core-ui';
-import React, { FunctionComponent } from 'react';
+import { Button } from '@viaa/avo2-components';
+import React, { FunctionComponent, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import MetaTags from 'react-meta-tags';
 
+import { DefaultSecureRouteProps } from '../../../authentication/components/SecuredRoute';
+import {
+	PermissionName,
+	PermissionService,
+} from '../../../authentication/helpers/permission-service';
 import { GENERATE_SITE_TITLE } from '../../../constants';
 import { withAdminCoreConfig } from '../../shared/hoc/with-admin-core-config';
-import { AdminLayout, AdminLayoutBody } from '../../shared/layouts';
+import { AdminLayout, AdminLayoutBody, AdminLayoutTopBarRight } from '../../shared/layouts';
+import { CONTENT_PAGE_PATH } from '../content-page.const';
 
-const ContentPageOverviewPage: FunctionComponent = () => {
+const { CREATE_CONTENT_PAGES } = PermissionName;
+
+const ContentPageOverviewPage: FunctionComponent<DefaultSecureRouteProps> = ({ history, user }) => {
 	const [t] = useTranslation();
+
+	const hasPerm = useCallback(
+		(permission: PermissionName) => PermissionService.hasPerm(user, permission),
+		[user]
+	);
 
 	const renderPageContent = () => {
 		return <ContentPageOverview />;
@@ -19,6 +33,17 @@ const ContentPageOverviewPage: FunctionComponent = () => {
 			pageTitle={t('admin/content-page/views/content-page-overview-page___contentoverzicht')}
 			size="full-width"
 		>
+			<AdminLayoutTopBarRight>
+				{hasPerm(CREATE_CONTENT_PAGES) && (
+					<Button
+						label={t('admin/content/views/content-overview___content-toevoegen')}
+						title={t(
+							'admin/content/views/content-overview___maak-een-nieuwe-content-pagina-aan'
+						)}
+						onClick={() => history.push(CONTENT_PAGE_PATH.CREATE)}
+					/>
+				)}
+			</AdminLayoutTopBarRight>
 			<AdminLayoutBody>
 				<MetaTags>
 					<title>
@@ -41,4 +66,4 @@ const ContentPageOverviewPage: FunctionComponent = () => {
 	);
 };
 
-export default withAdminCoreConfig(ContentPageOverviewPage);
+export default withAdminCoreConfig(ContentPageOverviewPage as FunctionComponent);
