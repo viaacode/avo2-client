f5d0cf7e86cd43b02bc180fe6e868a1946ab6c39
Ward Vercruyssen <wardverc@Wards-MacBook-Pro.local>
2022-11-24 09:56:26 +0100

    feat(AVO-2205): added modal to edit translation

--- src/admin/translations/views/TranslationsOverview.tsx
+++ src/admin/translations/views/TranslationsOverview.tsx
@@ -1,6 +1,6 @@
 import { TranslationsOverviewV2 } from '@meemoo/admin-core-ui';
+import { Modal } from '@meemoo/react-components';
 import { Button } from '@viaa/avo2-components';
-import clsx from 'clsx';
 import React, { FunctionComponent, ReactNode } from 'react';
 import { useTranslation } from 'react-i18next';
 import MetaTags from 'react-meta-tags';
@@ -10,26 +10,40 @@ import { GENERATE_SITE_TITLE } from '../../../constants';
 import { withAdminCoreConfig } from '../../shared/hoc/with-admin-core-config';
 import { AdminLayout, AdminLayoutBody } from '../../shared/layouts';
 
+import styles from './TranslationsOverviewV2.module.scss';
+
 const TranslationsOverview: FunctionComponent<DefaultSecureRouteProps> = () => {
 	const [t] = useTranslation();
 
 	const renderPopup = ({
+		title,
+		body,
+		isOpen,
 		onSave,
 		onClose,
 	}: {
-		title: ReactNode;
+		title: string;
 		body: ReactNode;
 		onSave: () => void;
 		onClose: () => void;
+		isOpen: boolean;
 	}) => {
+		const closeButton = () => {
+			return <Button icon="close" onClick={onClose} />;
+		};
+
+		const renderHeader = () => {
+			return (
+				<div className={styles['test']}>
+					{title}
+					{closeButton()}
+				</div>
+			);
+		};
+
 		const renderFooter = () => {
 			return (
-				<div
-					className={clsx(
-						'u-px-32 u-py-24'
-						//styles['c-translations-overview__blade-footer']
-					)}
-				>
+				<div className="u-px-32 u-py-24">
 					<Button
 						onClick={onSave}
 						label={t('pages/admin/vertalingen-v-2/index___bewaar-wijzigingen')}
@@ -43,7 +57,18 @@ const TranslationsOverview: FunctionComponent<DefaultSecureRouteProps> = () => {
 			);
 		};
 
-		return <div>{renderFooter()}</div>;
+		return (
+			<Modal
+				className={styles['c-translations-overview__modal']}
+				isOpen={isOpen}
+				closeButtonProps={{ hidden: true }}
+				heading={renderHeader()}
+				footer={renderFooter()}
+				onClose={onClose}
+			>
+				{body}
+			</Modal>
+		);
 	};
 
 	return (
@@ -67,7 +92,10 @@ const TranslationsOverview: FunctionComponent<DefaultSecureRouteProps> = () => {
 						)}
 					/>
 				</MetaTags>
-				<TranslationsOverviewV2 renderPopup={renderPopup} />
+				<TranslationsOverviewV2
+					renderPopup={renderPopup}
+					className={styles['c-translations-overview']}
+				/>
 			</AdminLayoutBody>
 		</AdminLayout>
 	);
