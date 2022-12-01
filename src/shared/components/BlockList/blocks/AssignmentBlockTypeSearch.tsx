276a499299e2bc7604d35979001f8bbed810e884
Ward Vercruyssen <wardverc@Wards-MacBook-Pro.local>
2022-12-01 15:03:47 +0100

    feat(AVO-2202): use NavigationOverview from admin-core

--- src/shared/components/BlockList/blocks/AssignmentBlockTypeSearch.tsx
+++ src/shared/components/BlockList/blocks/AssignmentBlockTypeSearch.tsx
@@ -14,8 +14,6 @@ import { BaseBlockWithMeta } from '../../../../assignment/assignment.types';
 import { CollectionFragmentRichText } from '../../../../collection/components';
 import useTranslation from '../../../../shared/hooks/useTranslation';
 
-import './AssignmentBlockTypeSearch.scss';
-
 export interface AssignmentBlockTypeSearchProps extends DefaultProps {
 	block: BaseBlockWithMeta;
 	showCollectionButton: boolean;
