c75b041787bf86c69d4a9577e2d32728b4d72f43
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2023-01-20 23:18:48 +0100

    fix(back-component): fix useTranslations import

--- src/admin/shared/components/Back/Back.tsx
+++ src/admin/shared/components/Back/Back.tsx
@@ -1,7 +1,7 @@
 import { Button, ButtonProps } from '@viaa/avo2-components';
 import React, { FC } from 'react';
 
-import useTranslation from '../../../shared/hooks/useTranslation';
+import useTranslation from '../../../../shared/hooks/useTranslation';
 
 export const Back: FC<ButtonProps> = (props) => {
 	const [t] = useTranslation();
