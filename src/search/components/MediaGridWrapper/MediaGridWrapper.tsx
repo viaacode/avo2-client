b994c034546080b19c2dfab072e1efdc0667c0ca
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2023-05-09 13:42:45 +0200

    fix(avo-2594): restore media tiles popup modal (wip)
    https://meemoo.atlassian.net/browse/AVO-2594

--- src/search/components/MediaGridWrapper/MediaGridWrapper.tsx
+++ src/search/components/MediaGridWrapper/MediaGridWrapper.tsx
@@ -4,18 +4,29 @@ import {
 	MediaGridBlockState,
 	MediaListItem,
 } from '@meemoo/admin-core-ui';
-import { ButtonAction, IconName, RenderLinkFunction } from '@viaa/avo2-components';
+import {
+	ButtonAction,
+	IconName,
+	Modal,
+	ModalBody,
+	RenderLinkFunction,
+} from '@viaa/avo2-components';
 import { Avo } from '@viaa/avo2-types';
-import { get, isEmpty, isNil } from 'lodash-es';
+import { isEmpty, isNil } from 'lodash-es';
 import React, { FunctionComponent, ReactNode, useCallback, useEffect, useState } from 'react';
 
 import { ContentPageService } from '../../../admin/content-page/services/content-page.service';
 import { ContentTypeString, toEnglishContentType } from '../../../collection/collection.types';
+import { APP_PATH } from '../../../constants';
+import { ItemVideoDescription } from '../../../item/components';
 import { LoadingErrorLoadedComponent, LoadingInfo } from '../../../shared/components';
 import { DEFAULT_AUDIO_STILL } from '../../../shared/constants';
-import { formatDate, isMobileWidth } from '../../../shared/helpers';
+import { buildLink, CustomError, formatDate, isMobileWidth } from '../../../shared/helpers';
+import { defaultRenderBookmarkButton } from '../../../shared/helpers/default-render-bookmark-button';
 import { parseIntOrDefault } from '../../../shared/helpers/parsers/number';
 import useTranslation from '../../../shared/hooks/useTranslation';
+import { BookmarksViewsPlaysService } from '../../../shared/services/bookmarks-views-plays-service';
+import { ToastService } from '../../../shared/services/toast-service';
 
 import { ResolvedItemOrCollection } from './MediaGridWrapper.types';
 
@@ -28,7 +39,6 @@ interface MediaGridWrapperProps extends MediaGridBlockState {
 	buttonAltTitle?: string;
 	ctaButtonAltTitle?: string;
 	commonUser: Avo.User.CommonUser;
-	mediaItemClicked: (mediaListItem: MediaListItem) => void;
 }
 
 const MediaGridWrapper: FunctionComponent<MediaGridWrapperProps> = ({
@@ -56,10 +66,9 @@ const MediaGridWrapper: FunctionComponent<MediaGridWrapperProps> = ({
 	results,
 	commonUser,
 	renderLink,
-	mediaItemClicked,
 }: any) => {
 	// TODO remove any when typings for admin-core-ui is fixed
-	const { tHtml } = useTranslation();
+	const { tText, tHtml } = useTranslation();
 
 	const [loadingInfo, setLoadingInfo] = useState<LoadingInfo>({ state: 'loading' });
 	const [resolvedResults, setResolvedResults] = useState<ResolvedItemOrCollection[] | null>(null);
@@ -67,6 +76,11 @@ const MediaGridWrapper: FunctionComponent<MediaGridWrapperProps> = ({
 	// cache search results
 	const [lastSearchQuery, setLastSearchQuery] = useState<string | null>(null);
 	const [lastSearchQueryLimit, setLastSearchQueryLimit] = useState<number | null>(null);
+
+	const [activeItem, setActiveItem] = useState<(Avo.Item.Item & ResolvedItemOrCollection) | null>(
+		null
+	);
+	const [activeItemBookmarkStatus, setActiveItemBookmarkStatus] = useState<boolean | null>(null);
 
 	const resolveMediaResults = useCallback(async () => {
 		try {
@@ -86,7 +100,7 @@ const MediaGridWrapper: FunctionComponent<MediaGridWrapperProps> = ({
 				const searchQueryLimitNumber =
 					parseIntOrDefault<undefined>(searchQueryLimit, undefined) || 8;
 				const searchQueryValue: string | null =
-					(get(searchQuery, 'value') as string | undefined) || null;
+					(searchQuery?.value as string | undefined) || null;
 
 				if (
 					(elements && elements.length && isNil(searchQueryValue)) ||
@@ -162,6 +176,61 @@ const MediaGridWrapper: FunctionComponent<MediaGridWrapperProps> = ({
 		}
 	}, [resolvedResults]);
 
+	const fetchActiveItemBookmarkStatus = useCallback(async () => {
+		if (!commonUser || !activeItem) {
+			return;
+		}
+		const statuses = await BookmarksViewsPlaysService.getBookmarkStatuses(
+			commonUser.profileId,
+			[
+				{
+					type: 'item',
+					uuid: activeItem.uid,
+				},
+			]
+		);
+		setActiveItemBookmarkStatus(statuses['item'][activeItem.uid]);
+	}, [activeItem?.external_id]);
+
+	useEffect(() => {
+		fetchActiveItemBookmarkStatus();
+	}, [fetchActiveItemBookmarkStatus]);
+
+	const toggleBookmark = async () => {
+		if (!commonUser || !activeItem || isNil(activeItemBookmarkStatus)) {
+			return;
+		}
+		try {
+			await BookmarksViewsPlaysService.toggleBookmark(
+				activeItem.uid,
+				commonUser,
+				'item',
+				activeItemBookmarkStatus
+			);
+
+			setActiveItemBookmarkStatus(!activeItemBookmarkStatus);
+			ToastService.success(
+				activeItemBookmarkStatus
+					? tText('De bladwijzer is verwijderd')
+					: tText('De bladwijzer is aangemaakt')
+			);
+		} catch (err) {
+			console.error(
+				new CustomError('Failed to toggle bookmark', err, {
+					commonUser,
+					itemId: activeItem.uid,
+					type: 'item',
+					activeItemBookmarkStatus,
+				})
+			);
+			ToastService.danger(
+				activeItemBookmarkStatus
+					? tText('Het verwijderen van de bladwijzer is mislukt')
+					: tText('Het aanmaken van de bladwijzer is mislukt')
+			);
+		}
+	};
+
 	const getThumbnailFromItem = (itemOrCollection: ResolvedItemOrCollection) => {
 		if (itemOrCollection.type?.label === 'audio') {
 			return DEFAULT_AUDIO_STILL;
@@ -204,12 +273,12 @@ const MediaGridWrapper: FunctionComponent<MediaGridWrapperProps> = ({
 				({
 					type: isItem ? 'ITEM' : isCollection ? 'COLLECTION' : 'BUNDLE',
 					value: itemOrCollection?.external_id || itemOrCollection?.id,
-					target: get(searchQuery, 'target') || '_self',
+					target: searchQuery?.target || '_self',
 				} as ButtonAction),
 			buttonAction: element.buttonAction,
 			title: itemOrCollection?.title || '',
 			description: itemOrCollection?.description || '',
-			issued: get(itemOrCollection, 'issued') || '',
+			issued: (itemOrCollection as Avo.Item.Item)?.issued || '',
 			organisation: itemOrCollection?.organisation || '',
 			thumbnail: {
 				label: itemLabel,
@@ -219,19 +288,42 @@ const MediaGridWrapper: FunctionComponent<MediaGridWrapperProps> = ({
 				src: getThumbnailFromItem(itemOrCollection),
 			},
 			src: itemOrCollection?.src,
-			item_collaterals: get(itemOrCollection, 'item_collaterals', null),
+			item_collaterals: (itemOrCollection as Avo.Item.Item)?.item_collaterals || null,
 		} as any;
 	};
 
 	const openInModal = (mediaListItem: MediaListItem): boolean => {
-		return openMediaInModal && get(mediaListItem, 'itemAction.type') === 'ITEM';
+		return openMediaInModal && mediaListItem?.itemAction?.type === 'ITEM';
 	};
 
 	const renderMediaCardWrapper = (mediaCard: ReactNode, item: MediaListItem) => {
 		if (openInModal(item)) {
-			return <a onClick={() => mediaItemClicked(item)}>{mediaCard}</a>;
+			return (
+				<a
+					onClick={() =>
+						setActiveItem(
+							(resolvedResults?.find((resultItem) => resultItem.src === item.src) ||
+								null) as Avo.Item.Item | null
+						)
+					}
+				>
+					{mediaCard}
+				</a>
+			);
 		}
 		return renderLink(item.itemAction, mediaCard, item.buttonAltTitle || item.title);
+	};
+
+	const renderBookmarkButton = (): ReactNode => {
+		if (!commonUser || isNil(activeItemBookmarkStatus)) {
+			return null;
+		}
+		return defaultRenderBookmarkButton({
+			active: activeItemBookmarkStatus,
+			ariaLabel: tText('Toggle bladwijzer'),
+			title: tText('Toggle bladwijzer'),
+			onClick: toggleBookmark,
+		});
 	};
 
 	// Render
@@ -262,6 +354,32 @@ const MediaGridWrapper: FunctionComponent<MediaGridWrapperProps> = ({
 					renderLink={renderLink}
 					renderMediaCardWrapper={renderMediaCardWrapper}
 				/>
+				<Modal
+					isOpen={!!activeItem}
+					onClose={() => {
+						setActiveItem(null);
+						setActiveItemBookmarkStatus(null);
+					}}
+					scrollable
+					size="medium"
+				>
+					<ModalBody>
+						{!!activeItem && !!activeItem.src && (
+							<ItemVideoDescription
+								src={activeItem.src}
+								poster={(activeItem as Avo.Item.Item)?.thumbnail_path}
+								itemMetaData={activeItem as unknown as Avo.Item.Item}
+								verticalLayout
+								showTitle
+								titleLink={buildLink(APP_PATH.ITEM_DETAIL.route, {
+									id: activeItem.external_id,
+								})}
+								collapseDescription={false}
+								renderButtons={renderBookmarkButton}
+							/>
+						)}
+					</ModalBody>
+				</Modal>
 			</>
 		);
 	};
