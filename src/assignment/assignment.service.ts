20638673746a7142dcbb14dd262f8ded60a7e994
SHDBramVandenbussche <bram.vandenbussche@studiohyperdrive.be>
2023-05-31 15:25:24 +0200

    feat(avo-2600): adjust assignements functions names

--- src/assignment/assignment.service.ts
+++ src/assignment/assignment.service.ts
@@ -1567,7 +1567,7 @@ export class AssignmentService {
 		}
 	}
 
-	static async addShareAssignmentUser(
+	static async addContributor(
 		assignmentId: string,
 		user: Partial<ContributorInfo>
 	): Promise<void> {
@@ -1607,10 +1607,7 @@ export class AssignmentService {
 		}
 	}
 
-	static async deleteShareAssignmentUser(
-		assignmentId: string,
-		contributorId: string
-	): Promise<void> {
+	static async deleteContributor(assignmentId: string, contributorId: string): Promise<void> {
 		try {
 			await fetchWithLogoutJson(
 				`${getEnv(
