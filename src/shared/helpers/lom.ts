b985ab9cb8bd35d029e5f50b6bcbe8fc083dad4e
SHDBramVandenbussche <bram.vandenbussche@studiohyperdrive.be>
2023-06-12 15:37:17 +0200

    feat(avo-2638): Update typings version and replace LomSchemeTypeEnum

--- src/shared/helpers/lom.ts
+++ src/shared/helpers/lom.ts
@@ -1,4 +1,4 @@
-import { Avo, LomSchemeTypeEnum } from '@viaa/avo2-types';
+import { Avo, LomSchemeType } from '@viaa/avo2-types';
 import { filter, groupBy, isNil } from 'lodash-es';
 
 import { LomFieldsByScheme } from '../types/lom';
@@ -11,11 +11,11 @@ export const groupLoms = (loms: { lom?: Avo.Lom.LomField }[]): LomFieldsByScheme
 
 	return {
 		educationLevels: filter(
-			groupedLoms[LomSchemeTypeEnum.structure],
+			groupedLoms[LomSchemeType.structure],
 			(lom) => !isNil(lom?.broader)
 		),
-		subjects: (groupedLoms[LomSchemeTypeEnum.subject] as Avo.Lom.LomField[]) || [],
-		themes: (groupedLoms[LomSchemeTypeEnum.theme] as Avo.Lom.LomField[]) || [],
-		contexts: filter(groupedLoms[LomSchemeTypeEnum.structure], { broader: null }),
+		subjects: (groupedLoms[LomSchemeType.subject] as Avo.Lom.LomField[]) || [],
+		themes: (groupedLoms[LomSchemeType.theme] as Avo.Lom.LomField[]) || [],
+		contexts: filter(groupedLoms[LomSchemeType.structure], { broader: null }),
 	} as LomFieldsByScheme;
 };
