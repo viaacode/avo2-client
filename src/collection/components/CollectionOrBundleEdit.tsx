c0ebf33f0cf52ce47f2c8d54d12eaddae546a137
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2023-07-14 18:34:40 +0200

    fix(avo-2776): hide toast message if statuscode indicates no access

--- src/collection/components/CollectionOrBundleEdit.tsx
+++ src/collection/components/CollectionOrBundleEdit.tsx
@@ -1056,13 +1056,15 @@ const CollectionOrBundleEdit: FunctionComponent<
 			if ((err as CustomError).innerException?.additionalInfo.statusCode === 409) {
 				await releaseCollectionEditStatus();
 				ToastService.danger(
-					tText(
+					tHtml(
 						'collection/components/collection-or-bundle-edit___iemand-is-deze-collectie-reeds-aan-het-bewerken'
 					)
 				);
+			} else if ((err as CustomError).innerException?.additionalInfo.statusCode === 401) {
+				return; // User has no rights to edit the collection
 			} else {
 				ToastService.danger(
-					tText(
+					tHtml(
 						'collection/components/collection-or-bundle-edit___verbinding-met-bewerk-server-verloren'
 					)
 				);
@@ -1076,7 +1078,7 @@ const CollectionOrBundleEdit: FunctionComponent<
 		} catch (err) {
 			if ((err as CustomError)?.innerException?.additionalInfo.statusCode !== 409) {
 				ToastService.danger(
-					tText(
+					tHtml(
 						'collection/components/collection-or-bundle-edit___er-liep-iets-fout-met-het-updaten-van-de-collectie-bewerk-status'
 					)
 				);
@@ -1094,7 +1096,7 @@ const CollectionOrBundleEdit: FunctionComponent<
 			await updateCollection();
 
 			ToastService.success(
-				tText(
+				tHtml(
 					'collection/components/collection-or-bundle-edit___je-was-meer-dan-15-minuten-inactief-je-aanpassingen-zijn-opgeslagen'
 				),
 				{
@@ -1103,7 +1105,7 @@ const CollectionOrBundleEdit: FunctionComponent<
 			);
 		} catch (err) {
 			ToastService.danger(
-				tText(
+				tHtml(
 					'collection/components/collection-or-bundle-edit___je-was-meer-dan-15-minuten-inactief-het-opslaan-van-je-aanpassingen-is-mislukt'
 				),
 				{
--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -20,9 +20,12 @@ export const ASSIGNMENT_OVERVIEW_BACK_BUTTON_FILTERS =
 export function getMoreOptionsLabel(): string {
 	return tText('shared/constants/index___meer-opties');
 }
+
 // Refetch interval of 15 seconds
 export const EDIT_STATUS_REFETCH_TIME = 15_000;
 // Initial time of 1 minute until warning modal pops up
-export const IDLE_TIME_UNTIL_WARNING = 60_000;
-// Max idle time of 14 minutes after warning modal pops up
-export const MAX_EDIT_IDLE_TIME = 840_000;
+export const IDLE_TIME_UNTIL_WARNING = 15_000;
+// export const IDLE_TIME_UNTIL_WARNING = 60_000;
+// Max idle time of 14 minutes after warning modal pops up (15 minutes total)
+export const MAX_EDIT_IDLE_TIME = 15_000;
+// export const MAX_EDIT_IDLE_TIME = 840_000;
