179f25422833e50d46502facbba074d9118b4b27
Ian Emsens <ian.emsens@studiohyperdrive.be>
2024-02-20 13:55:05 +0100

    fix(AVO-3099): add sorting

--- src/item/components/modals/AddToCollectionModal.tsx
+++ src/item/components/modals/AddToCollectionModal.tsx
@@ -73,7 +73,7 @@ const AddToCollectionModal: FunctionComponent<AddToCollectionModalProps & UserPr
 				user as Avo.User.User,
 				0,
 				500,
-				[],
+				{ created_at: 'desc' },
 				ContentTypeNumber.collection,
 				undefined,
 				undefined
