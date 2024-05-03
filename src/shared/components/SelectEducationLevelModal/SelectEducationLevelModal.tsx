f882c5b410f5211983b26a78b574c9f6714cc15b
Ian Emsens <ian.emsens@studiohyperdrive.be>
2024-05-03 13:22:28 +0200

    feat(AVO-3180): Create education level modal

---
+++ src/shared/components/SelectEducationLevelModal/SelectEducationLevelModal.tsx
@@ -0,0 +1,102 @@
+import './SelectEducationLevelModal.scss';
+
+import {
+	Button,
+	ButtonToolbar,
+	Modal,
+	ModalBody,
+	type ModalProps,
+	Select,
+	Toolbar,
+	ToolbarItem,
+	ToolbarRight,
+} from '@viaa/avo2-components';
+import { type LomFieldSchema } from '@viaa/avo2-types/types/lom';
+import React, { type FunctionComponent, useCallback, useMemo, useState } from 'react';
+
+import { EducationLevelId } from '../../helpers/lom';
+import { tHtml, tText } from '../../helpers/translate';
+import withUser, { type UserProps } from '../../hocs/withUser';
+import { useLomEducationLevels } from '../../hooks/useLomEducationLevels';
+
+type SelectEducationLevelModalProps = Omit<ModalProps, 'children'> &
+	Partial<UserProps> & {
+		onConfirm?: (lom: LomFieldSchema) => void;
+	};
+
+// Component
+
+const SelectEducationLevelModal: FunctionComponent<SelectEducationLevelModalProps> = (props) => {
+	const { user, commonUser, onConfirm, ...modal } = props;
+
+	const [educationLevels] = useLomEducationLevels();
+	const [selected, setSelected] = useState<LomFieldSchema | undefined>(undefined);
+
+	const options = useMemo(
+		() =>
+			[EducationLevelId.lagerOnderwijs, EducationLevelId.secundairOnderwijs]
+				.map((level) => educationLevels.find((item) => item.id === level))
+				.map(
+					(lom) =>
+						({
+							label: lom?.label,
+							value: lom?.id,
+						}) as { label: string; value: string }
+				),
+		[educationLevels]
+	);
+
+	const handleEducationLevelChange = useCallback(
+		(input: string) => {
+			const level = educationLevels.find(({ id }) => id === input);
+			setSelected(level);
+		},
+		[educationLevels, setSelected]
+	);
+
+	const handleConfirm = useCallback(() => {
+		selected && onConfirm?.(selected);
+	}, [selected, onConfirm]);
+
+	return (
+		<Modal title={tHtml('Kies je onderwijsniveau')} size="medium" scrollable {...modal}>
+			<ModalBody>
+				<section className="u-spacer-bottom">
+					{tHtml('Waarom is het belangrijk dat je een onderwijsniveau kiest?')}
+				</section>
+
+				<section className="u-spacer-bottom">
+					<Select
+						value={selected?.id}
+						onChange={handleEducationLevelChange}
+						options={options}
+					/>
+				</section>
+
+				<Toolbar>
+					<ToolbarRight>
+						<ToolbarItem>
+							<ButtonToolbar>
+								<Button
+									type="secondary"
+									label={tText('Annuleer')}
+									onClick={modal.onClose}
+								/>
+								<Button
+									type="primary"
+									label={tText('Kies onderwijsniveau')}
+									disabled={!selected}
+									onClick={handleConfirm}
+								/>
+							</ButtonToolbar>
+						</ToolbarItem>
+					</ToolbarRight>
+				</Toolbar>
+			</ModalBody>
+		</Modal>
+	);
+};
+
+export default withUser(
+	SelectEducationLevelModal
+) as FunctionComponent<SelectEducationLevelModalProps>;
--- src/shared/helpers/is-user.ts
+++ src/shared/helpers/is-user.ts
@@ -1,6 +1,8 @@
-import { CommonUserSchema } from '@viaa/avo2-types/types/user';
+import { type CommonUserSchema } from '@viaa/avo2-types/types/user';
 
 import { SpecialUserGroup } from '../../admin/user-groups/user-group.const';
+
+import { EducationLevelId } from './lom';
 
 export function isUserPupil(user: CommonUserSchema) {
 	return isUser(user, SpecialUserGroup.PupilSecondary);
@@ -8,6 +10,20 @@ export function isUserPupil(user: CommonUserSchema) {
 
 export function isUserElementaryPupil(user: CommonUserSchema) {
 	return isUser(user, SpecialUserGroup.PupilElementary);
+}
+
+export function isUserSecondaryTeacher(user: CommonUserSchema) {
+	return isUser(user, SpecialUserGroup.TeacherSecondary);
+}
+
+export function isUserDoubleTeacher(user: CommonUserSchema) {
+	// DoubleTeacher must always also be a SecondaryTeacher
+	if (!isUserSecondaryTeacher(user)) return false;
+
+	const levels = [EducationLevelId.lagerOnderwijs, EducationLevelId.secundairOnderwijs];
+	const hasLevels = levels.every((level) => user.loms.find(({ lom }) => lom?.id === level));
+
+	return hasLevels;
 }
 
 export function isUser(user?: CommonUserSchema, group?: SpecialUserGroup) {
