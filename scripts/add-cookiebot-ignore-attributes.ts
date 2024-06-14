717ae8d56c8a2bf546b0f9129e1cecbddd9a750a
Ian Emsens <ian.emsens@studiohyperdrive.be>
2024-06-14 13:26:45 +0200

    fix: adjust regex to capture more

--- scripts/add-cookiebot-ignore-attributes.ts
+++ scripts/add-cookiebot-ignore-attributes.ts
@@ -5,8 +5,8 @@ import replace, { type ReplaceResult } from 'replace-in-file';
 replace
 	.replaceInFile({
 		files: path.join(__dirname, '../dist/index.html'),
-		from: /<script[^>]*src="(\/assets\/index-[^.]+\.js)"[^>]*><\/script>/g,
-		to: '<script defer="defer" src="$1" data-cookieconsent="ignore"></script>',
+		from: /<script([^>]*)src="(\/assets\/index-[^.]+\.js)"([^>]*)><\/script>/g,
+		to: '<script$1src="$2"$3 data-cookieconsent="ignore"></script>',
 	})
 	.then((results: ReplaceResult[]) => {
 		console.info('Successfully added cookiebot ignore attributes:', results);
