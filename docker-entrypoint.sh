b0226d476c0a1703db9e764eb79dcd3ea598092c
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2024-07-10 12:58:02 +0200

    fix(AVO-3326): fix paths and equal comparison for robots.txt

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,13 +10,13 @@ sh ./env.sh
 # Copy the scripts/robots-QAS.txt file or the scripts/robots-PROD.txt file to public/robots.txt based on the ENV environment variable
 echo "copy robots.txt file"
 if  [[ "$ENV" == "qas" ]]; then
-		cp ./robots-QAS.txt ./robots.txt
+		cp $WD/robots-QAS.txt $WD/robots.txt
 else
-		cp ./robots-PRD.txt ./robots.txt
+		cp $WD/robots-PRD.txt $WD/robots.txt
 fi
 
 
-if  [[ "$1" -eq "bash" ]]; then
+if  [[ "$1" == "bash" ]]; then
     bash
 else nginx -g 'daemon off;'
 fi
