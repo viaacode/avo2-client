eb8eae5208e0f40feb8083befbc4eed26b4a3eaf
Ian Emsens <ian.emsens@studiohyperdrive.be>
2024-07-12 09:12:16 +0200

    fix(AVO-3327): pr remarks

--- src/shared/hooks/useDisablePictureInPicture.tsx
+++ src/shared/hooks/useDisablePictureInPicture.tsx
@@ -1,6 +1,12 @@
 import { type RefObject, useEffect } from 'react';
 
 export const useDisablePictureInPicture = (ref: RefObject<HTMLElement>) => {
+	const observer = new MutationObserver(() => {
+		Array.from(document.getElementsByTagName('video')).forEach((element) => {
+			element.disablePictureInPicture = true;
+		});
+	});
+
 	useEffect(() => {
 		if (ref.current) {
 			observer.observe(ref.current, {
@@ -9,12 +15,6 @@ export const useDisablePictureInPicture = (ref: RefObject<HTMLElement>) => {
 			});
 		}
 
-		return () => observer.disconnect();
-	});
-
-	const observer = new MutationObserver(() => {
-		Array.from(document.getElementsByTagName('video')).forEach((element) => {
-			element.disablePictureInPicture = true;
-		});
+		return observer.disconnect;
 	});
 };
