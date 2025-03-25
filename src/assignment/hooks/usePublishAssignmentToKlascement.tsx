a633e4efc2895491bf7e4e8f8c492aa28c171fcf
Femke Reunes <femke.reunes@studiohyperdrive.be>
2025-03-25 08:18:16 +0100

    AVO-3622: Publish assignment to klascement

--- src/assignment/hooks/usePublishAssignmentToKlascement.tsx
+++ src/assignment/hooks/usePublishAssignmentToKlascement.tsx
@@ -1,13 +1,9 @@
 import { useMutation } from '@tanstack/react-query';
 
-import {
-	type KlascementPublishAssignmentData,
-	KlascementService,
-} from '../../shared/services/klascement-service';
+import { KlascementService } from '../../shared/services/klascement-service';
 
 export const usePublishAssignmentToKlascement = () => {
 	return useMutation(
-		(publishData: KlascementPublishAssignmentData): Promise<number> =>
-			KlascementService.publishAssignment(publishData)
+		(publishData: string): Promise<number> => KlascementService.publishAssignment(publishData)
 	);
 };
