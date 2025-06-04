d6ec0a86495fd065e1485f2e5c0ec3e987f18248
Bert Verhelst <verhelstbert@gmail.com>
2025-06-04 14:03:37 +0200

    feat(AVO-3694): simplify embed url to /embed/<id>
    https://meemoo.atlassian.net/browse/AVO-3694

--- src/embed-code/helpers/links.ts
+++ src/embed-code/helpers/links.ts
@@ -1,15 +1,9 @@
-import { stringifyUrl } from 'query-string';
 import { generatePath } from 'react-router';
 
 import { APP_PATH } from '../../constants';
 
 export function toEmbedCodeIFrame(embedCodeId: string): string {
-	return stringifyUrl({
-		url: `${window.location.origin}/embed`,
-		query: {
-			embedId: embedCodeId,
-		},
-	});
+	return `${window.location.origin}/embed/${embedCodeId}`;
 }
 
 export function toEmbedCodeDetail(embedCodeId: string): string {
