b6872ea430890aca9642357f232b1dc1dd41b6ff
Femke Reunes <femke.reunes@studiohyperdrive.be>
2025-07-04 10:10:15 +0200

    Only refetch the login state if the user is not yet loggedIn

--- src/embed/hooks/useGetLoginStateForEmbed.ts
+++ src/embed/hooks/useGetLoginStateForEmbed.ts
@@ -6,7 +6,7 @@ import { EmbedCodeService } from '../../embed-code/embed-code-service';
 import { QUERY_KEYS } from '../../shared/constants/query-keys';
 import { CustomError } from '../../shared/helpers/custom-error';
 import { getEnv } from '../../shared/helpers/env';
-import store from '../../store';
+import store, { type AppState } from '../../store';
 import { LTI_JWT_TOKEN_HEADER } from '../embed.types';
 
 export const useGetLoginStateForEmbed = () => {
@@ -14,6 +14,12 @@ export const useGetLoginStateForEmbed = () => {
 		[QUERY_KEYS.GET_LOGIN_STATE_EMBED],
 		async () => {
 			try {
+				const loginState = (store.getState() as unknown as AppState)?.loginState.data;
+
+				if (loginState?.message === 'LOGGED_IN') {
+					return loginState;
+				}
+
 				const response = await fetch(`${getEnv('PROXY_URL')}/auth/check-login`, {
 					method: 'GET',
 					credentials: 'include',
