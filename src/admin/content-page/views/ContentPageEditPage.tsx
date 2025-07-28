1e8a7e1af1b42441bb2c183afbf776f9a48db787
Femke Reunes <femke.reunes@studiohyperdrive.be>
2025-07-28 14:15:09 +0200

    AVO-1256: add has changes modal to edit content pages

--- src/admin/content-page/views/ContentPageEditPage.tsx
+++ src/admin/content-page/views/ContentPageEditPage.tsx
@@ -1,13 +1,15 @@
 import type { ContentPageDetailProps } from '@meemoo/admin-core-ui/dist/admin.mjs';
 import { Flex, Spinner } from '@viaa/avo2-components';
-import React, { type FC, lazy, Suspense } from 'react';
+import React, { type FC, lazy, Suspense, useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'redux';
 
 import { type DefaultSecureRouteProps } from '../../../authentication/components/SecuredRoute';
+import { BeforeUnloadPrompt } from '../../../shared/components/BeforeUnloadPrompt/BeforeUnloadPrompt';
 import { buildLink } from '../../../shared/helpers/build-link';
 import { goBrowserBackWithFallback } from '../../../shared/helpers/go-browser-back-with-fallback';
 import withUser, { type UserProps } from '../../../shared/hocs/withUser';
+import { useWarningBeforeUnload } from '../../../shared/hooks/useWarningBeforeUnload';
 import { ADMIN_PATH } from '../../admin.const';
 import { withAdminCoreConfig } from '../../shared/hoc/with-admin-core-config';
 
@@ -21,6 +23,13 @@ const ContentPageDetailPage: FC<
 	DefaultSecureRouteProps<{ id: string }> & ContentPageDetailProps & UserProps
 > = ({ match, history, commonUser }) => {
 	const { id } = match.params;
+
+	const [hasUnsavedChanges] = useState<boolean>(true);
+
+	useWarningBeforeUnload({
+		when: hasUnsavedChanges,
+	});
+
 	return (
 		<Suspense
 			fallback={
@@ -40,6 +49,7 @@ const ContentPageDetailPage: FC<
 					)
 				}
 			/>
+			<BeforeUnloadPrompt when={hasUnsavedChanges} />
 		</Suspense>
 	);
 };
