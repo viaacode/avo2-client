ef4c4f2c00b833e03370fa6f7d5e4cf27c060c04
Femke Reunes <femke.reunes@studiohyperdrive.be>
2025-08-01 10:17:55 +0200

    AVO-3537: make the local storage key uniform with other keys

--- src/authentication/helpers/login-options-preferred-tab.ts
+++ src/authentication/helpers/login-options-preferred-tab.ts
@@ -1,6 +1,6 @@
 import { NOT_NOW_LOCAL_STORAGE_KEY } from '../../shared/constants';
 
-const LoginOptionsPreferredTabStorageKey = 'LoginOptions.preference';
+const LOGIN_OPTIONS_PREFERRED_TAB_LOCAL_STORAGE_KEY = 'AVO.login_options_preferred_tab';
 
 export const LoginOptionsTabs = {
 	TEACHER: 'lesgever',
@@ -8,11 +8,14 @@ export const LoginOptionsTabs = {
 };
 
 export function getPreferredLoginOption() {
-	return localStorage?.getItem(LoginOptionsPreferredTabStorageKey) || LoginOptionsTabs.TEACHER;
+	return (
+		localStorage?.getItem(LOGIN_OPTIONS_PREFERRED_TAB_LOCAL_STORAGE_KEY) ||
+		LoginOptionsTabs.TEACHER
+	);
 }
 
 export function setPreferredLoginOption(id: string | number) {
-	localStorage.setItem(LoginOptionsPreferredTabStorageKey, id.toString());
+	localStorage.setItem(LOGIN_OPTIONS_PREFERRED_TAB_LOCAL_STORAGE_KEY, id.toString());
 }
 
 export function removePreferredLoginOption() {
