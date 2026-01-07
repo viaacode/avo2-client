5c1c1328003011668933fe71ce13ecf6e8e2b108
Bert Verhelst <verhelstbert@gmail.com>
2026-01-07 19:38:45 +0100

    fix(AVO-3804): fix bugs with ssr: localstorage, hor scroll item detail

--- src/assignment/helpers/links.ts
+++ src/assignment/helpers/links.ts
@@ -2,19 +2,38 @@ import { AvoAssignmentAssignment } from '@viaa/avo2-types';
 
 import { APP_PATH } from '../../constants';
 import { ASSIGNMENT_OVERVIEW_BACK_BUTTON_FILTERS } from '../../shared/constants';
-import { buildLink } from '../../shared/helpers/build-link';
+import { buildLink, RouteParams } from '../../shared/helpers/build-link';
+import { CustomError } from '../../shared/helpers/custom-error.ts';
+import { isServerSideRendering } from '../../shared/helpers/routing/is-server-side-rendering.ts';
 import { ASSIGNMENTS_ID } from '../../workspace/workspace.const';
 import { ASSIGNMENT_CREATE_UPDATE_TABS } from '../assignment.const';
 
 export function backToOverview(): string {
+  let backButtonFilters = {};
+  if (!isServerSideRendering()) {
+    // Server side rendering doesn't have access to local storage
+    try {
+      backButtonFilters = JSON.parse(
+        localStorage.getItem(ASSIGNMENT_OVERVIEW_BACK_BUTTON_FILTERS) || '{}',
+      );
+    } catch (err) {
+      console.error(
+        new CustomError(
+          'Failed to parse back button filters from local storage',
+          err,
+          {
+            key: ASSIGNMENT_OVERVIEW_BACK_BUTTON_FILTERS,
+          },
+        ),
+      );
+    }
+  }
   return buildLink(
     APP_PATH.WORKSPACE_TAB.route,
     {
       tabId: ASSIGNMENTS_ID,
     },
-    JSON.parse(
-      localStorage.getItem(ASSIGNMENT_OVERVIEW_BACK_BUTTON_FILTERS) || '{}',
-    ),
+    backButtonFilters,
   );
 }
 
