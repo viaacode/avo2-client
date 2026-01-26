fe2fe3c28cdefb7191ef2d803a318c5856fa1106
Bert Verhelst <verhelstbert@gmail.com>
2026-01-26 11:41:58 +0100

    fix(AVO-3879): check for not filled in using isNil for assignment detail
    https://meemoo.atlassian.net/browse/AVO-3879

--- src/assignment/helpers/is-item-with-meta.ts
+++ src/assignment/helpers/is-item-with-meta.ts
@@ -1,9 +1,10 @@
 import { AvoCoreBlockItemBase, AvoItemItem } from '@viaa/avo2-types';
+import { isNil } from 'es-toolkit';
 
 export const isItemWithMeta = (block: AvoCoreBlockItemBase): boolean => {
   if (block.type === 'ITEM') {
     if (block.item_meta) {
-      return (block.item_meta as AvoItemItem).depublish_reason === undefined;
+      return isNil((block.item_meta as AvoItemItem).depublish_reason);
     } else {
       return false;
     }
