09a8d997d3b188ede71ab3e6606c2b605141bd81
Bert Verhelst <verhelstbert@gmail.com>
2026-02-03 13:28:30 +0100

    fix(ssr): avoid using window in getBaseUrl during ssr

--- src/authentication/helpers/get-base-url.ts
+++ src/authentication/helpers/get-base-url.ts
@@ -1,7 +1,12 @@
 import { trimEnd } from 'es-toolkit';
 import { type Location } from 'react-router';
+import { getEnv } from '../../shared/helpers/env.ts';
+import { isServerSideRendering } from '../../shared/helpers/routing/is-server-side-rendering.ts';
 
 export function getBaseUrl(location: Location): string {
+  if (isServerSideRendering()) {
+    return getEnv('CLIENT_URL') as string;
+  }
   if (location.pathname === '/') {
     return trimEnd(window.location.href, '/');
   }
